73b7e0a922529f46a68d490724258d05d624cb14
Dhanush <bhdhanushshetty@gmail.com>
2022-06-14 17:19:32 +0530

    Revert "00 Test resolved"
    This reverts commit 0276b531761415f1134be10f2c77eb278940281a.

--- Tasks/pybasics/basics/test_00_understanding_assert.py
+++ Tasks/pybasics/basics/test_00_understanding_assert.py
@@ -1,8 +1,8 @@
 __author__ = 'Hari'
 
-#from placeholders import *
+from placeholders import *
 
-NOTES = '''
+notes = '''
 This lesson introduces the basic assert statement in python. assert is generally used to 'assert' the truth of an
 expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is raised with
 the <optional message>. If is evaluates to True, nothing happens.
@@ -13,37 +13,32 @@ the <optional message>. If is evaluates to True, nothing happens.
 
 def test_assert_true():
     #throws assertion error
-    "davvad"
     assert True  #This should be True -- replace ___ with True.
 
 def test_assert_true_with_message():
-    "bhbhbj"
-    assert True #"This is the failure message"#replace_with True to stop seeing the assertion error
+    assert True, "This is the failure message" # replace ___ with True to stop seeing the assertion error
 
 def test_assert_equality():
-    "savfds"
     assert 7 == 2 + 5   #replace __ with the expected value
 
 #Fill in __ in the statements below to make the asserts succeed
 def test_make_assert_true_1():
-    "ddsv"
-    i=8
-    assert i>7 #"Fill in a value greater than 7"
+    assert 8>7, "Fill in a value greater than 7"
 
 #you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
-    "dfvas"
-    assert 2**43 > 2**30 #"Fill in value greater than 2**30"
+    assert 2**43 > 2**30, "Fill in value greater than 2**30"
 
 def test_make_assert_true_3():
-    "evaaaaaaew"
-    s_1 = "Hello, World"
-    s_2 = "Hello, World"
-    assert s_1 == s_2
+    s1 = "Hello, World"
+    s2 = "Hello, World"
+    assert s1 == s2
 
-THREE_THINGS_I_LEARNT = """
+three_things_i_learnt = """
 asseret ==bool
--comparing state
--commenting
+-
+-
 """
-TIME_TAKEN_MINUTES = 1
+
+time_taken_minutes = 1
+
