d751f7c1243ebb12d877c0d95ad4d148d057b027
Aishwarya <aishaishwarya914@gmail.com>
2022-06-14 16:56:18 +0530

    added random comments

--- Tasks/pybasics/basics/test_00_understanding_assert.py
+++ Tasks/pybasics/basics/test_00_understanding_assert.py
@@ -9,34 +9,29 @@ the <optional message>. If is evaluates to True, nothing happens.
 
  In the tests below, replace the blanks with values so that the resulting expression is True.
 '''
-
+# added random comment
 
 def test_assert_true():
     #throws assertion error
-    "davvad"
     assert True  #This should be True -- replace ___ with True.
 
 def test_assert_true_with_message():
-    "bhbhbj"
     assert True #"This is the failure message"#replace_with True to stop seeing the assertion error
 
 def test_assert_equality():
-    "savfds"
     assert 7 == 2 + 5   #replace __ with the expected value
 
 #Fill in __ in the statements below to make the asserts succeed
 def test_make_assert_true_1():
-    "ddsv"
     i=8
     assert i>7 #"Fill in a value greater than 7"
 
 #you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
-    "dfvas"
     assert 2**43 > 2**30 #"Fill in value greater than 2**30"
 
 def test_make_assert_true_3():
-    "evaaaaaaew"
+    
     s_1 = "Hello, World"
     s_2 = "Hello, World"
     assert s_1 == s_2
