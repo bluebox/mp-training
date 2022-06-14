d67a56cc9d1413593b5990c8e12bdff4abab1d1c
Dhanush <bhdhanushshetty@gmail.com>
2022-06-14 16:18:09 +0530

    00 Test resolved

--- Tasks/pybasics/basics/test_00_understanding_assert.py
+++ Tasks/pybasics/basics/test_00_understanding_assert.py
@@ -1,8 +1,8 @@
 __author__ = 'Hari'
 
-from placeholders import *
+#from placeholders import *
 
-notes = '''
+NOTES = '''
 This lesson introduces the basic assert statement in python. assert is generally used to 'assert' the truth of an
 expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is raised with
 the <optional message>. If is evaluates to True, nothing happens.
@@ -13,32 +13,37 @@ the <optional message>. If is evaluates to True, nothing happens.
 
 def test_assert_true():
     #throws assertion error
+    "davvad"
     assert True  #This should be True -- replace ___ with True.
 
 def test_assert_true_with_message():
-    assert True, "This is the failure message" # replace ___ with True to stop seeing the assertion error
+    "bhbhbj"
+    assert True #"This is the failure message"#replace_with True to stop seeing the assertion error
 
 def test_assert_equality():
+    "savfds"
     assert 7 == 2 + 5   #replace __ with the expected value
 
 #Fill in __ in the statements below to make the asserts succeed
 def test_make_assert_true_1():
-    assert 8>7, "Fill in a value greater than 7"
+    "ddsv"
+    i=8
+    assert i>7 #"Fill in a value greater than 7"
 
 #you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
-    assert 2**43 > 2**30, "Fill in value greater than 2**30"
+    "dfvas"
+    assert 2**43 > 2**30 #"Fill in value greater than 2**30"
 
 def test_make_assert_true_3():
-    s1 = "Hello, World"
-    s2 = "Hello, World"
-    assert s1 == s2
+    "evaaaaaaew"
+    s_1 = "Hello, World"
+    s_2 = "Hello, World"
+    assert s_1 == s_2
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 asseret ==bool
--
--
+-comparing state
+-commenting
 """
-
-time_taken_minutes = 1
-
+TIME_TAKEN_MINUTES = 1
