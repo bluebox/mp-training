4a72a0dc96fd25568472ff32e64ff39a06fdcacb
shashi-kiran-reddy <shashikiranzz99@gmail.com>
2022-06-16 14:51:22 +0530

    test00

--- tasks/basics/test_01_understanding_numbers.py
+++ tasks/basics/test_01_understanding_numbers.py
@@ -1,6 +1,6 @@
 __author__ = 'Hari'
 
-from tasks.placeholders import *
+from tasks.placeholders import __author__
 
 
 # For most of these tests use the interpreter to fill up the blanks.
@@ -13,7 +13,8 @@ def test_numbers_types():
 # In Python 3 the long datatype has been removed and all integer values are handled
 # by the Int class. The default size of Int will depend on your CPU architecture.
 # 32 bit systems the default datatype for integers will be 'Int32'->[-2147483648,2147483647]
-# 64 bit systems the default datatype for integers will be 'Int64'->[-9223372036854775808,9223372036854775807]
+# 64 bit systems the default datatype for integers
+#  will be 'Int64'->[-9223372036854775808,9223372036854775807]
 
 
 def test_numbers_int_arithmetic_operations():
@@ -42,10 +43,10 @@ def test_numbers_long():
     assert 1606938044258990275541962092341162602522202993782792835301376 == 2 ** 200
 
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 -we have a method in python to convert a decimal number to octal
 - a method to convert decimal tp hexadecemal
 -
 """
 
-time_taken_minutes = 4
+TIME_TAKEN_MINUTES = 4
--- tasks/basics/test_02_understanding_strings.py
+++ tasks/basics/test_02_understanding_strings.py
@@ -1,40 +1,41 @@
 __author__ = 'Hari'
 
-from tasks.placeholders import *
+from tasks.basics.test_00_understanding_assert import NOTES
+from tasks.placeholders import __author__
 
-notes = """string is one of the most commonly used data types, it has different
+NOTES = """string is one of the most commonly used data types, it has different
 behavior than a char* in C."""
 
 def test_string_type():
     assert 'str'== type("Hello World").__name__
-    assert True== isinstance("Hello World", str)
+    assert True is isinstance('Hello World',str)
 
 def test_single_quoted_strings_are_strings():
-    assert True== isinstance('Hello World', str)
+    assert True is isinstance('Hello World',str)
 
 def test_double_quoted_strings_are_strings():
-    assert True== isinstance("Hello World", str)
+    assert True is isinstance("Hello World",str)
 
 def test_triple_quoted_strings_are_strings():
-    assert True== isinstance("""Hello World""", str)
+    assert True is isinstance("""Hello World""",str)
 
 def test_triple_single_quoted_strings_are_strings():
-    assert True== isinstance('''Hello World''', str)
+    assert True is isinstance('''Hello World''',str)
 
 def test_raw_strings_are_strings():
-    assert True == isinstance(r"Hello World", str)
+    assert True is isinstance(r"hello World",str)
 
 def test_single_quoted_strings_can_have_double_quotes():
     first = 'The pilot said "Jump"'
     second = "The pilot said \"Jump\""  #note back slash escaping of "
     are_equal = (first == second)
-    assert True== are_equal
+    assert True is are_equal== are_equal
 
 def test_double_quoted_strings_can_have_single_quotes():
     first = "The pilot said 'Jump'"
     second = 'The pilot said \'Jump\''  #note back slash escaping of '
     are_equal = (first == second)
-    assert True== are_equal
+    assert True is are_equal
 
 def test_triple_quoted_strings_can_have_both_single_and_double_quotes():
     """ Edit tq_str to make are_equal True """
@@ -47,7 +48,7 @@ def test_triple_quoted_strings_can_span_lines():
     tq_str = """Hello
 World"""
     dq_str = "Hello\nWorld"   # what is the double quoted form of tq_str
-    assert (tq_str == dq_str)
+    assert tq_str == dq_str
 
 def test_string_len():
     assert 13 == len("Hello 'world'")
@@ -74,9 +75,9 @@ def test_strings_can_be_indexed():
     assert 'H' == string[-0]  # hint -0 is 0
     assert  5 == len(string)
     try:
-        out_of_bounds = string[5] #raises an error, we will revisit exceptions later
-    except IndexError as ie:
-        print (ie)   #string index out of range
+        string[5] #raises an error, we will revisit exceptions later
+    except IndexError as i_e:
+        print (i_e)   #string index out of range
         assert True  #make this True to proceed.
 
 def test_chars_are_strings_too():
@@ -91,8 +92,8 @@ def test_strings_are_immutable():
     string = "Hello"
     try:
         string[0] = "M"
-    except TypeError as te:
-        print (te)
+    except TypeError as t_e:
+        print (t_e)
         assert True
 
 def test_string_concat():
@@ -153,15 +154,16 @@ def test_string_formatting():
     assert 'Ravi is 25 years old'== stmt
 
 def test_string_membership():
-    assert False== ('c' in 'apple')  #is there a precedence issue here?
-    assert True == ('a' in 'apple')
-    assert  True== ('app' in 'apple')  # '==' and 'in' operators have same precedence are interpreted from left to right in the expression
+    assert False is 'c' in 'apple'  #is there a precedence issue here?
+    assert True is 'a' in 'apple'
+    assert  True is 'app' in 'apple'# '==' and 'in' operators have
+    # same precedence are interpreted from left to right in the expression
 
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 -strings are immutable
 -strings can be accessed through indexing
 -strings can be sliced
 """
 
-time_taken_minutes = 20
+TIME_TAKEN_MINUTES = 20
