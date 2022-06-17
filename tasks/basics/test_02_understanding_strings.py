2ff450f58fbaf8a09d79f32eb7ba854262e88753
ShazwanA <shazwanali911081@gmail.com>
2022-06-17 18:13:21 +0530

    pyling done

--- tasks/basics/test_02_understanding_strings.py
+++ tasks/basics/test_02_understanding_strings.py
@@ -1,65 +1,82 @@
+'''string assertion'''
+
 __author__ = 'Hari'
 
-from tasks.placeholders import *
+#from tasks.basics.test_00_understanding_assert import TIME_TAKEN_MINUTES
+#from stringprep import c22_specials
+from tasks.placeholders import __author__, __all__, __, ___
 
-notes = """string is one of the most commonly used data types, it has different
+NOTES = """string is one of the most commonly used data types, it has different
 behavior than a char* in C."""
 
 def test_string_type():
+    '''string assertion'''
     assert 'str'== type("Hello World").__name__
-    assert True== isinstance("Hello World", str)
+    assert True is isinstance("Hello World", str)
 
 def test_single_quoted_strings_are_strings():
-    assert True== isinstance('Hello World', str)
+    '''quoted string assertion'''
+    assert True is isinstance('Hello World', str)
 
 def test_double_quoted_strings_are_strings():
-    assert True== isinstance("Hello World", str)
+    '''double quoted string assertion'''
+    assert True is isinstance("Hello World", str)
 
 def test_triple_quoted_strings_are_strings():
-    assert True== isinstance("""Hello World""", str)
+    '''triple quoted string assertion'''
+    assert True is isinstance("""Hello World""", str)
 
 def test_triple_single_quoted_strings_are_strings():
-    assert True== isinstance('''Hello World''', str)
+    '''triple quoted string assertion'''
+    assert True is isinstance('''Hello World''', str)
 
 def test_raw_strings_are_strings():
-    assert True == isinstance(r"Hello World", str)
+    '''raw string assertion'''
+    assert True is isinstance(r"Hello World", str)
 
 def test_single_quoted_strings_can_have_double_quotes():
+    '''single quote can have double quote'''
     first = 'The pilot said "Jump"'
     second = "The pilot said \"Jump\""  #note back slash escaping of "
     are_equal = (first == second)
-    assert True== are_equal
+    assert True is are_equal
 
 def test_double_quoted_strings_can_have_single_quotes():
+    '''double quote can have single quote'''
     first = "The pilot said 'Jump'"
     second = 'The pilot said \'Jump\''  #note back slash escaping of '
     are_equal = (first == second)
-    assert True== are_equal
+    assert True is are_equal
 
 def test_triple_quoted_strings_can_have_both_single_and_double_quotes():
-    """ Edit tq_str to make are_equal True """
+    '''triple quote can have both single and double'''
+
     tq_str = """ Isn't the "Hobbit" great? """
     dq_str = "Isn't the \"Hobbit\" great?"
     are_equal = (tq_str == dq_str)
-    assert  False == are_equal
+    assert  False is are_equal
 
 def test_triple_quoted_strings_can_span_lines():
+    '''span line'''
     tq_str = """Hello
 World"""
     dq_str = "Hello\nWorld"   # what is the double quoted form of tq_str
-    assert (tq_str == dq_str)
+    assert True is (tq_str == dq_str)
 
 def test_string_len():
+    '''string len'''
     assert 13 == len("Hello 'world'")
     assert 13 == len('Hello \'world\'')
 
-def test_triple_quoted_strings_can_span_lines():
+def test_triple_quoted_strings_can_span_lines1():
+    '''triple quote can have span line'''
     string = """Hello
     World"""
-    assert True == isinstance(string, str)
+    assert True is isinstance(string, str)
     assert 15 == len(string)
 
 def test_strings_can_be_indexed():
+    '''string can be indexed'''
     string = "Hello"
     assert 'H'== string[0]
     assert 'e'== string[1]
@@ -75,11 +92,13 @@ def test_strings_can_be_indexed():
     assert  5 == len(string)
     try:
         out_of_bounds = string[5] #raises an error, we will revisit exceptions later
-    except IndexError as ie:
-        print (ie)   #string index out of range
+        print(out_of_bounds)
+    except IndexError as i_e:
+        print (i_e)   #string index out of range
         assert True  #make this True to proceed.
 
 def test_chars_are_strings_too():
+    '''chars are also string'''
     string = "Hello"
     first_char = string[0]
     assert 'str' == type(first_char).__name__
@@ -87,22 +106,24 @@ def test_chars_are_strings_too():
     assert 'str'== type("a").__name__
 
 def test_strings_are_immutable():
-    """ strings in python cannot be modified unlike in C """
+    '''strings are immutable'''
     string = "Hello"
     try:
         string[0] = "M"
-    except TypeError as te:
-        print (te)
+    except TypeError as t_e:
+        print (t_e)
         assert True
 
 def test_string_concat():
+    '''string concatenation'''
     assert "Hello  world"== "Hello " + " world"
     assert 'Hello world'== """Hello """ + 'world'
     assert 'Hello world'== 'Hello ' + "world"
 
 
 def test_string_slicing():
-    """ Slicing creates new strings """
+    '''string slicing'''
+
     string = "Hello world"
     #with begin : end
     assert ''== string[0:0]
@@ -130,38 +151,41 @@ def test_string_slicing():
 
 
 def test_string_repeat():
+    '''repeatition of string'''
     assert 'HelloHelloHello' == "Hello" * 3
     assert 12 == len("Hello " * 2)
 
 def test_string_combine():
-    """
-    Use slicing to pass the assert.
-    """
+    '''addition of strings'''
     hello = "Hello World"
     bye = "Goodbye moon"
     assert  bye[0:8] + hello[6:]  == "Goodbye World"
 
 def test_string_formatting():
-    greeting = "Hello '{0}'".format("learner")
+    '''string formatting'''
+    greeting = "Hello {0}".format("learner")
     assert "Hello 'learner'"== greeting
 
     truth = "{1} plus {1} makes {0}".format("two","one")
     assert truth == 'one plus one makes two'
 
-
     stmt = "{name} is {age} years old".format(name="Ravi", age=25)
     assert 'Ravi is 25 years old'== stmt
 
 def test_string_membership():
-    assert False== ('c' in 'apple')  #is there a precedence issue here?
-    assert True == ('a' in 'apple')
-    assert  True== ('app' in 'apple')  # '==' and 'in' operators have same precedence are interpreted from left to right in the expression
+    '''membership in string using in operator'''
+    string='apple'
+    c_1='c'
+    c_2='a'
+    c_3='app'
+    assert False is (c_1 in string)  #is there a precedence issue here?
+    assert True is (c_2 in 'apple')
+    assert  True is (c_3 in 'apple')
 
-
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 -strings are immutable
 -strings can be accessed through indexing
 -strings can be sliced
 """
 
-time_taken_minutes = 20
+TIME_TAKEN_MINUTES = 20
