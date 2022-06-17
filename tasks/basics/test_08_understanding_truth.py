e54f601ebc29d0bcd5f213190e66aa7084a3d5b8
sahithi <aendapallysahithireddy@gmail.com>
2022-06-17 10:51:43 +0530

    test 08 pylint commit

--- tasks/basics/test_08_understanding_truth.py
+++ tasks/basics/test_08_understanding_truth.py
@@ -1,8 +1,8 @@
 __author__ = 'Hari'
 
-from tasks.placeholders import *
+from tasks.placeholders import __author__
 
-notes = '''
+NOTES = '''
 Just like C, python has notions on what values are considered true
 and what values are considered false.
 
@@ -10,7 +10,8 @@ Assigning truth equivalence to non-bool types leads to much more
 elegant way of writing code instead of having explicit comparisons
 with base values of the data types like 0, '', [] etc.
 '''
-
+zero=0
+one=1
 #None is a first class object in python
 def test_none_type():
     assert "NoneType" == type(None).__name__
@@ -30,11 +31,10 @@ def test_truth_none():
     assert "false" == result
 
 # a helper function used to test the truth value of an object.
-def truth_test(object, description):
-    if object:
+def truth_test(obj, description):
+    if obj:
         return description + " is treated as true"
-    else:
-        return description + " is treated as false"
+    return description + " is treated as false"
 
 def test_truth_values():
     assert "empty string is treated as false" == truth_test("", "empty string")
@@ -55,27 +55,26 @@ def test_truth_values():
 # The fact that certain things are treated as True or False by
 # control flow statements does not mean that they are equal to True or False.
 def test_equality():
-    assert False == ("" == True)
-    assert False == (() == True)
-    assert False == ([] == True)
-    assert False == (set() == True)
-    assert False == (0 == True)
-    assert False == ("" == False)
-    assert False == (() == False)
-    assert False == ([] == False)
-    assert False == (set() == False)
-    assert True == (0 == False)
-    assert True == (1 == True)
-    assert False == ("a" == True)
-    assert False == ((1,2) == True)
-    assert False == ([1] == True)
-    assert False == ({1} == True)
+    assert False is ("" is True)
+    assert False is (() is True)
+    assert False is ([] is True)
+    assert False is (set() is True)
+    assert False is (zero is True)
+    assert False is ("" is False)
+    assert False is (() is False)
+    assert False is ([] is False)
+    assert False is (set() is False)
+    assert True is (zero == False)
+    assert True is (one == True)
+    assert False is ("a" is True)
+    assert False is ((1,2) is True)
+    assert False is ([1] is True)
+    assert False is ({1} is True)
 
-
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT= """
 -
 -
 -
 """
 
-time_taken_minutes = 30
+TIME_TAKEN_MINUTES = 30
