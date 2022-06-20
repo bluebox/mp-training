8d77f24167a68b0515f5101acb78c52e8bae5545
sairamyadhav <sairamyadhav4@gmail.com>
2022-06-20 16:20:50 +0530

    test_01

--- tasks/basics/test_01_understanding_numbers.py
+++ tasks/basics/test_01_understanding_numbers.py
@@ -10,7 +10,7 @@ def test_numbers_types():
     assert 'int' == type(1).__name__
     assert 'float' == type(1.3).__name__
 # assert 'int' == type(10L).__name__
-# In Python 3 the long datatype has been removed and all integer values are handled
+# In Python 3 the long datatype has been removed and all integer values are handled.
 # by the Int class. The default size of Int will depend on your CPU architecture.
 # 32 bit systems the default datatype for integers will be 'Int32'->[-2147483648,2147483647]
 # 64 bit systems the default datatype for integers will be 'Int64'->[-9223372036854775808,9223372036854775807]
