0c794aa19d94fe74d352d4d0f0addd56a2b8df4c
sairamyadhav <sairamyadhav4@gmail.com>
2022-06-20 18:30:23 +0530

    test_18

--- tasks/basics/test_18_understanding_nested_functions.py
+++ tasks/basics/test_18_understanding_nested_functions.py
@@ -1,6 +1,6 @@
 __author__ = 'Hari'
 
-notes = '''
+NOTES = '''
 nested functions underlie many advanced features of python. So a basic understanding of this
 feature is essential to mastering python.
 
@@ -19,50 +19,50 @@ def test_inner_func_scope():
     # inner_func not accessible by default
     try:
         inner_func()
-    except Exception:  # fill up the exception
-        pass
+    except NameError as n_e:  # fill up the exception
+        print(n_e)
 
     # this syntax does not work either, it is not just static scoping.
     try:
         outer_func.inner_func()
-    except Exception : # fillup the exception
-        pass
+    except AttributeError as a_e : # fillup the exception
+        print(a_e)
 
 def test_inner_func_can_be_returned():
-    f1 = outer_func(10)
-    assert 'function' == type(f1).__name__
-    assert 30 == f1(20)
+    f_1 = outer_func(10)
+    assert 'function' == type(f_1).__name__
+    assert 30 == f_1(20)
 
 def test_each_invocation_returns_a_new_func():
-    f1 = outer_func(10)
-    f2 = outer_func(10)
+    f_1 = outer_func(10)
+    f_2 = outer_func(10)
 
-    assert False == (f1 is f2)
-    assert False == (f1 == f2)
+    assert False is (f_1 is f_2)
+    assert False is (f_1 == f_2)
 
-    f3 = f2
-    assert True == (f3 is f2)
-    assert True == (f3 == f2)
+    f_3 = f_2
+    assert True is (f_3 is f_2)
+    assert True is (f_3 == f_2)
 
 def test_inner_func_has_access_to_outer_variables_after_return():
-    f1 = outer_func(20)
-    f2 = outer_func(50)
+    f_1 = outer_func(20)
+    f_2 = outer_func(50)
 
-    assert 50 == f1(30)
-    assert 60 == f1(40)
+    assert 50 == f_1(30)
+    assert 60 == f_1(40)
 
-    assert 80 == f2(30)
-    assert 90 == f2(40)
+    assert 80 == f_2(30)
+    assert 90 == f_2(40)
 
 def print_attributes(obj):
-    for x in dir(obj):
+    for x_1 in dir(obj):
         print("attribute: {0}".format(x))
-        print(getattr(obj, x))
+        print(getattr(obj, x_1))
 
 
 def test_inner_func_attributes():
-    f1 = outer_func(10)
-    assert 36 == len(dir(f1)) #how many attributes does f1 have
+    f_1 = outer_func(10)
+    assert 35 == len(dir(f_1)) #how many attributes does f1 have
 
     # use the print_attributes function to explore the properties
     # fill up the attribute name that you think holds a reference to the
@@ -74,9 +74,9 @@ def test_inner_func_attributes():
 def test_inner_func_scoping():
     def outer():
         funcs = []
-        for x in range(10):
+        for x_1 in range(10):
             def inner():
-                return x
+                return x_1
             funcs.append(inner)
         result = []
         for func in funcs:
@@ -87,17 +87,17 @@ def test_inner_func_scoping():
 
 # generally you should not write code like this :), this is only to learn
 def test_outer_scope_is_read_only():
-    y = 30
-    def outer(x):
+    y_1 = 30
+    def outer(x_1):
         def inner1():
-            x = 30
-            return x
+            x_1 = 30
+            return x_1
         def inner2():
-            return x + y
+            return x_1 + y_1
         def inner3():
-            y = 10
-            return x + y
-        return [inner1(), inner2(), inner3(), x, y]
+            y_1 = 10
+            return x_1 + y_1
+        return [inner1(), inner2(), inner3(), x_1, y_1]
 
     assert [30, 50, 30, 20, 30] == outer(20)
 
@@ -105,23 +105,23 @@ def test_outer_scope_is_read_only():
 # binding to a code object! So same scope rules as variables apply to function names.
 # read up more at http://effbot.org/zone/default-values.htm
 def test_def_is_a_statement():
-    def outer(x):
-        if x > 10:
-            def f():
-                return x * 2
+    def outer(x_1):
+        if x_1 > 10:
+            def f_1():
+                return x_1 * 2
         else:
-            def f():
-                return x * 3
-        return f
+            def f_1():
+                return x_1 * 3
+        return f_1
 
     assert 40 == outer(20)()
     assert 15 == outer(5)()
 
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 -
 -
 -
 """
 
-time_taken_minutes = ___
+TIME_TAKEN_MINUTES = ___
