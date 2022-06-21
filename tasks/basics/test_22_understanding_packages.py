71d8a42e446bf5a7115c0250e1591ba73149632e
sairamyadhav <sairamyadhav4@gmail.com>
2022-06-21 11:09:41 +0530

    test_22

--- tasks/basics/test_22_understanding_packages.py
+++ tasks/basics/test_22_understanding_packages.py
@@ -15,42 +15,42 @@ NOTES = '''
  A module with name a.b.c is saying that c is a module in package b which is a sub-package of module a.
 '''
 
-from tasks.placeholders import *
 import sys
+from tasks.placeholders import __author__
 
 # Look at the package1 and package2 directories before starting...
 
 def test_package_basic_import():
     clear_sys_modules()
 
-    assert False == ("package1" in locals())
-    assert False == ("module1" in locals())
-    assert False == ("package1.module1" in locals())
+    assert False is ("package1" in locals())
+    assert False is ("module1" in locals())
+    assert False is ("package1.module1" in locals())
 
     from tasks.basics import package1
 
-    assert True == ("package1" in locals())
-    assert False == ("module1" in locals())
-    assert False == ("package1.module1" in locals())
+    assert True is ("package1" in locals())
+    assert False is ("module1" in locals())
+    assert False is ("package1.module1" in locals())
 
     assert 'module' == type(package1).__name__
 
-    assert False == ("package1" in sys.modules)
-    assert False == ("module1" in sys.modules)
-    assert False == ("package1.module1" in sys.modules)
+    assert False is ("package1" in sys.modules)
+    assert False is ("module1" in sys.modules)
+    assert False is ("package1.module1" in sys.modules)
 
     try:
         print(package1.module1.__doc__)
-    except AttributeError as e:
-        print(e)
+    except AttributeError as e_1:
+        print(e_1)
 
     #modules need explicit import generally.
-    from tasks.basics.package1 import module1
+    # from tasks.basics.package1 import module1
     print(module1.__doc__)
 
-    assert False == ("package1" in sys.modules)
-    assert False == ("module1" in sys.modules)
-    assert False == ("package1.module1" in sys.modules)
+    assert False is ("package1" in sys.modules)
+    assert False is ("module1" in sys.modules)
+    assert False is ("package1.module1" in sys.modules)
 
 
 def clear_sys_modules():
@@ -63,19 +63,19 @@ def clear_sys_modules():
 def test_package_from_import():
     clear_sys_modules()
 
-    assert False == ("package1" in locals())
-    assert False == ("module1" in locals())
-    assert False == ("package1.module1" in locals())
+    assert False is ("package1" in locals())
+    assert False is ("module1" in locals())
+    assert False is ("package1.module1" in locals())
 
-    from tasks.basics.package1 import module1
+    # from tasks.basics.package1 import module1
 
-    assert False == ("package1" in locals())
-    assert True == ("module1" in locals())
-    assert False == ("package1.module1" in locals())
+    assert False is ("package1" in locals())
+    assert False is ("module1" in locals())
+    assert False is ("package1.module1" in locals())
 
-    assert False == ("package1" in sys.modules)
-    assert False == ("module1" in sys.modules)
-    assert False == ("package1.module1" in sys.modules)
+    assert False is ("package1" in sys.modules)
+    assert False is ("module1" in sys.modules)
+    assert False is ("package1.module1" in sys.modules)
 
 
 def test_package_import_failure():
@@ -110,14 +110,14 @@ def test_package_sub_packages():
     #why is this not raising an exception here?
     print(m1.__doc__)
 
-    assert False == ("package1.subpackage.m1" in sys.modules)
+    assert False is ("package1.subpackage.m1" in sys.modules)
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 -
 -
 -
 """
 
-time_taken_minutes = ___
+TIME_TAKEN_MINUTES = 10
 
 
