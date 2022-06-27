75cecc7eb6407dab342824dede988e4b95dae8cc
Shashi-kiran-reddy <shashikiranzz99@gmail.com>
2022-06-27 18:26:24 +0530

    minute upgradations can   be done in payment function

--- tasks/oops/parkinglot/__init__.py
+++ tasks/oops/parkinglot/__init__.py
@@ -28,7 +28,7 @@ v1 = Lot(15, "car", 10, 10, 5, 1, "audi")
 obj = v1
 v2=Lot(20,'bus',10,10,5,1,"ashoka_leyland")
 obj=v2
-v3=Lot(20,'bike',10,10,5,1,'trumph')
+v3=Lot(20,'bike',10,11,5,1,'trumph')
 v3.in_time=5
 obj=v3
 
@@ -36,14 +36,17 @@ obj=v3
 def allocation(obj):
     """this is the function where we allocate/park our vechile"""
     c_r=checkforspace(obj)
-    if(c_r!=None):
-       if obj.name=='car':
-          j=0
-       elif obj.name=='bus':
-          j= 1
-       else:
-          j= 2
-       Lot.series[c_r][j]=Lot.series[c_r][j]-obj.widthv
+    if c_r is not None:
+        if c_r is not False:
+            if obj.name=='car':
+                j=0
+            elif obj.name=='bus':
+                j= 1
+            else:
+                j= 2
+            Lot.series[c_r][j]=Lot.series[c_r][j]-obj.widthv
+        else:
+            print("no space")
     else:
         print("no space")
 def checkforspace(obj):
@@ -53,23 +56,17 @@ def checkforspace(obj):
             print('no space available')
             return False
         else:
-            c_h = checkseries(obj)
-        # print('no space available')
-            pass
+            c_h = checkseries(obj)            
     if obj.name == "bus":
         if obj.widthv>30:
-            print('no space available')
             return False
         else:
-            c_h = checkseries(obj)
-            pass
+            c_h = checkseries(obj)            
     if obj.name== "bike":
         if obj.widthv>10:
-            print('no space available')
             return False
         else:
-            c_h = checkseries(obj)
-            pass
+            c_h = checkseries(obj)           
     return c_h
 def checkseries(obj):
     """function to check wheather the series contains required space or not"""
@@ -102,15 +99,3 @@ def payment(obj):
     elif total_parking_time>=2 and total_parking_time<=10:
         print("pay",end=' ')
         print(total_parking_time*10+10)
-allocation(obj)
-print(obj.series['A'])
-#deallocation('A',obj)
-#print(obj.series['A'])
-#payment(obj)
-allocation(obj)
-print(obj.series['B'])
-allocation(obj)
-print(obj.series['C'])
-allocation(obj)
-allocation(obj)
-allocation(obj)
