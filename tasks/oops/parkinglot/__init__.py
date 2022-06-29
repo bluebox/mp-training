2f636607c714e1af4b5e85c41288a58c32113d6e
samba2001 <reddy200101@gmail.com>
2022-06-29 10:52:03 +0530

    single file

--- tasks/oops/parkinglot/__init__.py
+++ tasks/oops/parkinglot/__init__.py
@@ -1,6 +1,119 @@
-from vehicle import Vehicle
-from time import time
-from parking import park,unpark
+'''main code '''
+class Vehicle:
+    '''a class vehicle '''
+    def __init__(self,v_milage,v_name,v_capacity,v_width,v_depth,v_number,v_manufacturer,v_time):
+        self.v_name=v_name
+        self.v_milage=v_milage
+        self.v_capacity=v_capacity
+        self.v_width=v_width
+        self.v_manufacturer=v_manufacturer
+        self.v_depth=v_depth
+        self.v_number=v_number
+        self.v_time=v_time
+    def __str__(self):
+        '''a method '''
+        return self.v_name
+
+class series:
+    '''series is aclass'''
+    def __init__(self):
+        self.total_area=500000
+        self.car_area=self.total_area*0.6
+        self.bike_area = self.total_area * 0.3
+        self.bus_area = self.total_area * 0.1
+        self.list1=[]
+
+
+    def __str__(self) :
+        '''it returns a string message of an object of class '''
+        return "series"
+    
+
+
+a_series=series()
+b_series=series()
+c_series=series()
+d_series=series()
+def bill(intime,outtime):
+    '''this function is used to generate bill '''
+    total_time=intime-outtime
+    if(total_time<=1) :
+        return 20
+    elif(total_time<=10) :
+        return (20*10)+10
+    return 100+(total_time-10)*5
+
+def park(obj,time,type,area):
+    '''this function is used to park '''
+    if(type==1):
+        if a_series.car_area>area :
+            print(a_series.car_area)
+            a_series.car_area-=area
+
+            a_series.list1.append(obj)
+        elif b_series.car_area>area :
+            print(b_series.car_area)
+            b_series.car_area-=area
+            b_series.list1.append(obj)
+        elif c_series.car_area>area :
+            print(c_series.car_area)
+            c_series.car_area-=area
+            c_series.list1.append(obj)
+        elif d_series.car_area>area :
+            print(d_series.car_area)
+            d_series.car_area-=area
+            d_series.list1.append(obj)
+        else :
+            print("there is no space to park your car")
+    elif(type==2):
+        if a_series.bike_area>area :
+            a_series.bike_area-=area
+            a_series.list1.append(obj)
+        elif b_series.bike_area>area:
+            b_series.bike_area-=area
+            b_series.list1.append(obj)
+        elif c_series.bike_area>area:
+            c_series.bike_area-=area
+            c_series.list1.append(obj)
+        elif d_series.bike_area>area:
+            d_series.bike_area-=area
+            d_series.list1.append(obj)
+        else:
+            print("there is no space to park your bike")
+    elif type==3 :
+        if a_series.bus_area>area:
+            a_series.bus_area-=area
+            a_series.list1.append(obj)
+        elif b_series.bus_area>area:
+            b_series.bus_area-=area
+            b_series.list1.append(obj)
+        elif c_series.bus_area>area:
+             c_series.bus_area-=area
+             c_series.list1.append(obj)
+        elif d_series.bus_area>area:
+            d_series.bus_area-=area
+            d_series.list1.append(obj)
+        else:
+            print("there is no space to park your bus")
+
+
+total_lot=[a_series,b_series,c_series,d_series]
+def unpark(type,num):
+    '''this function is used to unpark vehicle'''
+    for ele in total_lot:
+        for obj in ele.list1:
+            if (obj.number==num):
+                area=obj.depth*obj.width
+                print("totla bill is {0} ".format(bill(10,33.5)))
+                if(type==1):
+                    ele.car_area-=area
+                elif (type==2):
+                    ele.bike_area-=area
+                elif(type==3):
+                    ele.bus_area-=area
+                break
+
+
 
 
 while True:
