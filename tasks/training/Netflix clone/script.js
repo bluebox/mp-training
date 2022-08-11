438433b74d015db0ecee3a3d753fa020fab220ca
Arun-Kumar-Reddy-Padala <arunkumarreddypadala@gmail.com>
2022-08-11 11:14:29 +0530

    Netflix clone

---
+++ tasks/training/Netflix clone/script.js
@@ -0,0 +1,29 @@
+
+var myIndex=0;
+carousel();
+
+
+function carousel(){
+    var i;
+    var x=document.getElementsByClassName("imageSlides");
+    for (i=0;i<x.length;i++){
+        x[i].style.display="none"
+    }
+    myIndex++
+    if(myIndex>x.length){
+        myIndex=1
+    }
+    x[myIndex-1].style.display="block";
+    setTimeout(carousel,5000);
+
+}
+
+
+
+function onClick() {
+    window.open("page1.html");
+  }
+
+function openPage(){
+    window.open("signin.html","_self");
+}
