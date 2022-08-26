70932765538968c23f9872be5dc7c8c76186e36d
shyam@216143 <vennushyamkumar@gmail.com>
2022-08-26 13:20:07 +0530

    submit project

---
+++ tasks/django/django/basicproject/basicapp/converter1.py
@@ -0,0 +1,8 @@
+class myconverter:
+    regex = '[A-Z]{3}[0-9]{4}'
+
+    def to_python(self, v1):
+        return v1
+
+    def to_url(self, v1):
+        return v1
---
+++ tasks/django/django/basicproject/basicapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, register_converter, re_path
+from . views import index, home, parameter1, int_parameter, nested_int_parameter, regex_parameter,comments, repath, nested_repath
+from .converter1 import myconverter
+register_converter(myconverter, 'shyam')
+urlpatterns = [
+    path('', index),
+    path('home', home),
+    path('parameter/<parameter>', parameter1),
+    path('int_parameter/<int:parameter>', int_parameter),
+    path('nested_int_parameter/<int:parameter1>/<int:parameter2>/<int:parameter3>', nested_int_parameter),
+    path('regex_parameter/<shyam:parameter>', regex_parameter),
+    re_path(r'^repath/(?P<name>[0-9]{4}[a-z]{2})/$', repath),
+    re_path('nested_repath/(?P<name1>[0-9]{4})/(?P<name2>[0-9]{2})/(?P<name3>[0-9]{2})', nested_repath),
+    re_path(r'nested_comments/(?:Rama-(?P<number>[0-9]+)/)?$', comments),
+    ]
+
+
+
