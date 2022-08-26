5f606f54a98fc963a3e7a5b594844cf2a714df14
irfan7839 <73037785+irfan7839@users.noreply.github.com>
2022-08-26 11:03:48 +0530

    Add files via upload
    Django-url-tasks

---
+++ Django-tasks/testproject/myapp/converter.py
@@ -0,0 +1,6 @@
+class UserNameConverter:
+    regex = 'MED[0-9]{4}'
+    def to_python(self,value):
+        return value
+    def to_url(self, value):
+        return value
---
+++ Django-tasks/testproject/myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def twoparameter(request, parameter1,parameter2):
+    html=""
+    html= "this is my app " + str(parameter1) + "" + str(parameter2)
+    print(parameter1)
+    return HttpResponse(html)
+
+# passing extra options from url to view to stop conflict
+def oneparameter(request, parameter1,user_id):
+    html = ""
+    html = "this is my app " + str(parameter1)
+    print(parameter1)
+    return HttpResponse(html)
+
+def intparameter(request, parameter):
+    html = ""
+    html = "this is my app " + str(parameter)
+    print(parameter)
+    return HttpResponse(html)
+def uuidparameter(request,parameter):
+    html = ""
+    html = "this is my app " + str(parameter)
+    print(parameter)
+    return HttpResponse(html)
+
+def slugparameter(request,parameter):
+    html = ""
+    html = "this is my app " + str(parameter)
+    print(parameter)
+    return HttpResponse(html)
+
