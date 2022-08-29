c8a717bd4c6aaf47254ae17f0634b14835afa218
shashi-kiran-reddy <shashikiranzz99@gmail.com>
2022-08-29 11:32:19 +0530

    django course on templates

--- temp/views.py
+++ temp/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 def index(request):
      return  render(request,'temp/index.html',{'name':'shashi'}) #variables
 def tags(request):
-     return render(request,'temp/tags.html',{'a': 1})
+     return render(request,'temp/tags.html',{'a':1,'players':['virat','sachin']})
 def filters(request):
      return render(request,'temp/filters.html',{'a' : 2})
+
