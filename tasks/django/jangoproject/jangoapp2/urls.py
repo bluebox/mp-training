fc2765e9e429a2d7b3f5b3de9776361e3e0dcb02
Manasaveerla <veerlamanasa15@gmail.com>
2022-08-29 13:25:52 +0530

    templates practice

--- tasks/django/jangoproject/jangoapp2/urls.py
+++ tasks/django/jangoproject/jangoapp2/urls.py
@@ -4,4 +4,10 @@ urlpatterns = [
     path('path1/',views.index,name='index'),
 
     path('path2/',views.temp1,name='temp1'),
+
+    path('path3/',views.temp2,name = 'temp2'),
+
+    path('path4/',views.temp3,name = 'temp3'),
+
+    path('path5/',views.temp4,name = 'temp4')
 ]
--- tasks/django/jangoproject/jangoapp2/views.py
+++ tasks/django/jangoproject/jangoapp2/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
@@ -5,3 +6,33 @@ def index(request):
 
 def temp1(request):
     return render(request,'jangoapp2/index.html')
+
+def temp2(request):
+    context = {
+        'name' : 'manasa',
+        'age':21,
+        'dept' : ['ece','mpc','general'],
+        'id' : {'btech':'18TR1A0489','inter': 'bme13','ssc':'5'},
+        'series' : (2,4,6,8,10)
+    }
+    return render(request,'jangoapp2/temp2.html',context)
+
+def temp3(request):
+    context = {
+        'data' : [1,2,3,4,5]
+    }
+    return render(request,'jangoapp2/temp3.html',context)
+
+def temp4(request):
+    context = {
+        'name1' : 'manasa',
+        'name2' : 'aishwarya',
+        'name3' : 'rakesh',
+        'name4' : 'venkatesh',
+        'name5' : 'arun',
+        'name6' : 'shazwan ali',
+        'name7' : 'mydayisgoodday'
+    }
+    return render(request,'jangoapp2/temp4.html',context)
+
+
