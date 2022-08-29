5915593c0fbc3c3f12377edb426813ba8a1fc003
bandaru nikas <bandarunikas3@gmail.com>
2022-08-29 12:50:17 +0530

    checksave

--- tasks/django_training/django_training/training_django/training_django/urls.py
+++ tasks/django_training/django_training/training_django/training_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('turls/',include('turls.urls')),
+    path('tview/',include('tview.urls')),
 ]
