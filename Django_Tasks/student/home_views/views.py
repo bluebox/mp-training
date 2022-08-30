8575c6ce563115c4bfd53090326807b411860176
purnima101 <purnimaagarwal2013@gmail.com>
2022-08-30 15:46:37 +0530

    day 3

--- Django_Tasks/student/home_views/views.py
+++ Django_Tasks/student/home_views/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.views import View
+import json
 # Create your views here.
 
 def index(request):
@@ -11,11 +12,19 @@ def index(request):
 
 
 class ViewEx(View):
+
     def get(self,request):
-        return HttpResponse("Heyyyyyyyy get")
+        return HttpResponse("Heyyyyyyyy get",status=405)
 
     def post(self,request):
         return HttpResponse("Heyyyyyyyy post")
+
+    def delete(self, request):
+        context = {
+            'name': 'Purnima'
+        }
+        return HttpResponse(json.dumps(context),content_type="application/json")
+
 
 
 
