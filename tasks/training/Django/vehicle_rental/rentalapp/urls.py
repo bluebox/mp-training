0b4190c18f08477b7fce535c130e17a692742c6b
Dhanush B H <107237163+DhanushShetty96@users.noreply.github.com>
2022-09-09 18:17:15 +0530

    Django project models comiitted

--- tasks/training/Django/vehicle_rental/rentalapp/urls.py
+++ tasks/training/Django/vehicle_rental/rentalapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login')
+    path('customer_login', views.customer_login, name='customer_login'),
+    path('owner_login', views.owner_login, name='owner_login')
 ]
--- tasks/training/Django/vehicle_rental/rentalapp/views.py
+++ tasks/training/Django/vehicle_rental/rentalapp/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html' )
-def login(request):
-    return render(request, 'login.html')
+
+def customer_login(request):
+    return render(request, 'customer_login.html')
+
+def owner_login(request):
+    return render(request, 'owner_login.html')
