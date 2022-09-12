4c5dc7b417d481b0ef89f1bf42db99e5986085b0
Manasaveerla <veerlamanasa15@gmail.com>
2022-09-12 12:12:30 +0530

    mysql connected

--- tasks/managementsystem/salon_project/salon_project/settings.py
+++ tasks/managementsystem/salon_project/salon_project/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'salon_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/mphs/Desktop/mp-training/tasks/managementsystem/salon_project/my.cnf',
+        },
     }
 }
 
