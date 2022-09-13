e9e1c6e98af0af516c30e75f50c40044aefcc502
AjayBolgam <ajaygoud8003@gmail.com>
2022-09-13 17:00:05 +0530

    adding Validators ORM

--- tasks/training/ormtask/tasks/models.py
+++ tasks/training/ormtask/tasks/models.py
@@ -7,3 +7,6 @@ class ValidatorsTask(models.Model):
     uid = models.IntegerField(primary_key=True)
     uname = models.CharField(max_length=50)
     upass = models.CharField(max_length=50, validators=[validatePassword])  # works with ModelForm
+
+    def __str__(self):
+        return self.uname
--- tasks/training/ormtask/tasks/validators.py
+++ tasks/training/ormtask/tasks/validators.py
@@ -1,10 +1,14 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-
+import re
 # Create your models here.
 
 
 def validatePassword(value):
 
     if len(value) < 8 :
-        raise ValidationError(_(' value error of  number'), params={'value': value})
+        regcap = re.compile(r'[A-Z]+')
+        reglow = re.compile(r'[a-z]+')
+        regdig = re.compile(r'[0-9]+')
+        if not regcap.search(value) and not reglow.search(value) and not regdig.search(value):
+            raise ValidationError(_(' value error of  number'), params={'value': value})
