4b0c328ae64ab02af57e190c48e51b578ffd5902
purnima101 <purnimaagarwal2013@gmail.com>
2022-09-13 11:23:25 +0530

    Reversed engineered schema

--- Django_Tasks/student/OrderFood/models.py
+++ Django_Tasks/student/OrderFood/models.py
@@ -8,8 +8,6 @@ from django.db import models
 # Create your models here.
 
 
-
-
 class Restaurant(models.Model):
     restaurant_id=models.CharField(max_length=10,primary_key=True)
     restaurant_name=models.CharField(max_length=30)
