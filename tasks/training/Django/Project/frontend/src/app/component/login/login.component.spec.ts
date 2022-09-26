8881c58f6709253df2f47648417edeb726636280
Dhanush B H <107237163+DhanushShetty96@users.noreply.github.com>
2022-09-26 18:36:01 +0530

    changes

--- tasks/training/Django/Project/frontend/src/app/component/login/login.component.spec.ts
+++ tasks/training/Django/Project/frontend/src/app/component/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { OwnerloginComponent } from './ownerlogin.component';
+import { LoginComponent } from './login.component';
 
-describe('OwnerloginComponent', () => {
-  let component: OwnerloginComponent;
-  let fixture: ComponentFixture<OwnerloginComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OwnerloginComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(OwnerloginComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
