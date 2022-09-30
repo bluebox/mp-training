bd1a5fd4f9197d3a295419669bce9469effea0f9
shyam@216143 <vennushyamkumar@gmail.com>
2022-09-30 14:39:17 +0530

    updated

--- tasks/Django work/social media _app/Social_Media_App/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, posting, likepost,followerscount
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(posting)
-admin.site.register(likepost)
-admin.site.register(followerscount)
--- tasks/Django work/social media _app/Social_Media_App/router.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .consumer import Mysync1, Myasync
-
-websocket_urlpatterns = [
-    path('ws/as/', Myasync.as_asgi()),
-    path('ws/sc/', Mysync1.as_asgi())
-]
