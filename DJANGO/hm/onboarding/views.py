af80acf05c04490ad4305f54496aa6e79aae46a8
saksham <thakursaksham252@gmail.com>
2022-09-30 18:35:20 +0530

    website

--- DJANGO/hm/onboarding/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from login.models import*
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
--- DJANGO/hm/recruitment/admin.py
+++
@@ -1,8 +0,0 @@
-from .models import*
-from django.contrib import admin
-
-admin.site.register(applying_candidate)
-admin.site.register(assesment)
-admin.site.register(vaacancy)
-
-
