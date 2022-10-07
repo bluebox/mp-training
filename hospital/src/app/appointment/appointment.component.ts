d810daf7870bf51cffe14eec7290fa9e90058f09
Sakshi <sakshidubey4467@gmail.com>
2022-10-07 19:48:17 +0530

    functions added

--- hospital/src/app/appointment/appointment.component.ts
+++ hospital/src/app/appointment/appointment.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ServercomunicationService } from '../servercomunication.service';
 
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
-  styleUrls: ['./appointment.component.css']
+  styleUrls: ['./appointment.component.css'],
+  providers:[ServercomunicationService]
 })
 export class AppointmentComponent implements OnInit {
-
-  constructor() { }
+  form_appointment: FormGroup = new FormGroup({});
+  doctor: any;
+  constructor(private fb: FormBuilder,private api:ServercomunicationService) { }
 
   ngOnInit(): void {
+    this.getData();
+
+    console.log(this.getData());
   }
+  getData()
+{
+  this.api.getAllDoctor().subscribe(
+    (data)=>{
+      console.log(data);
+      this.doctor=data;
+      console.log(this.doctor[0].d_id)
+    },
+    error=>{
+
+  console.log(error);
+
+  // this.service.getAPatient().subscribe((data: Patient[])=>this.patients=data[0])
+  // console.log(this.patients)
+})
+}
 
 }
--- hospital/src/app/servercomunication.service.ts
+++ hospital/src/app/servercomunication.service.ts
@@ -9,26 +9,83 @@ import { Patient } from './interfaces/patients';
 })
 export class ServercomunicationService {
 
-  url="http://127.0.0.1:8000/patient/dont";
+  url="http://127.0.0.1:8000/";
   httpHeaders= new HttpHeaders({'Content-Type':'application/json'});
 
   constructor( private http: HttpClient) {}
 
   getAllPatient():Observable<any>
   {
-    console.log('hi')
-    return this.http.get(this.url+'/patient/',{headers: this.httpHeaders});
+    console.log('patient list get')
+    return this.http.get(this.url+'patient/dont/patient/',{headers: this.httpHeaders});
   }
-  // getAPatient(id: any):Observable<any>
-  // {
-  //   console.log('one patient')
-  //   return this.http.get('${url}/${id}',{headers: this.httpHeaders});
-  // }
+  getAllDoctor():Observable<any>
+  {
+    console.log('doctor list get')
+    return this.http.get(this.url+'doctor/doctor/',{headers: this.httpHeaders});
+  }
+  getAllBills():Observable<any>
+  {
+    console.log('bill list get')
+    return this.http.get(this.url+'/bill/bill/',{headers: this.httpHeaders});
+  }
+  getAllAppointment():Observable<any>
+  {
+    console.log('doctors apointment list get')
+    return this.http.get(this.url+'appointment/appointment',{headers: this.httpHeaders});
+  }
+  getAllSalary():Observable<any>
+  {
+    console.log('salary list get')
+    return this.http.get(this.url+'salary/salary/',{headers: this.httpHeaders});
+  }
+  getAllDiagnosis():Observable<any>
+  {
+    console.log('diagnosis list get')
+    return this.http.get(this.url+'diagnosis/diagnosis/',{headers: this.httpHeaders});
+  }
+  getAllSlot():Observable<any>
+  {
+    console.log('diagnosis list get')
+    return this.http.get(this.url+'slot/slot/',{headers: this.httpHeaders});
+  }
+  getASlot(id: any):Observable<any>
+  {
+    console.log('diagnosis list get')
+    return this.http.get(`${this.url}slot/slot/${id}`,{headers: this.httpHeaders});
+  }
+  getAPatient(id: any):Observable<any>
+  {
+    console.log('one patient')
+    return this.http.get('${this.url}patient/dont/patient/${id}',{headers: this.httpHeaders});
+  }
+  getAApointment(id: any):Observable<any>
+  {
+    console.log('one patient')
+    return this.http.get('${this.url}appointment/appointment/${id}',{headers: this.httpHeaders});
+  }
 
   register_patient(data: any) {
-      return this.http.post(this.url+'/patient/', data,{headers: this.httpHeaders});
+      return this.http.post(this.url+'patient/dont/patient/', data,{headers: this.httpHeaders});
   }
-
+  register_doctor(data: any) {
+    return this.http.post(this.url+'doctor/doctor/', data,{headers: this.httpHeaders});
+}
+register_staff(data: any) {
+  return this.http.post(this.url+'staff/staff/', data,{headers: this.httpHeaders});
+}
+register_appointment(data: any) {
+  return this.http.post(this.url+'appointment/appointment/', data,{headers: this.httpHeaders});
+}
+register_bill(data: any) {
+  return this.http.post(this.url+'bill/bill/', data,{headers: this.httpHeaders});
+}
+register_diagnosis(data: any) {
+  return this.http.post(this.url+'diagnosis/diagnosis/', data,{headers: this.httpHeaders});
+}
+register_slot(data: any) {
+  return this.http.post(this.url+'slot/slot/', data,{headers: this.httpHeaders});
+}
   //   update(id: any, data: any) {
   //     return this.http.put(`${baseUrl}/${id}`, data);
   //   }
