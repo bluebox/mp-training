16416fc80caea6d4059d81896de3a5e3ea5ea7e9
Aishwarya <61001094+AishwaryaDayanand@users.noreply.github.com>
2022-10-17 10:41:32 +0530

    changes

--- tasks/training/project/diagnostic_django/appointment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AppointmentBooking, getEmployees
-urlpatterns = [
-    path('book-appointment/' ,AppointmentBooking.as_view(),name='book-appointment' ),
-    path('get-employee/' ,getEmployees,name='get-employee' ),
-
-]
-
-
