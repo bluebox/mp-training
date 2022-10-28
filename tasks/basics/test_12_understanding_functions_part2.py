8502dc1f28987371c9b0898b7bfb5036a17c5501
SoAbhious <106592121+SoAbhious@users.noreply.github.com>
2022-10-28 12:33:41 +0530

    Updated test_12_understanding_functions_part2

--- tasks/basics/test_12_understanding_functions_part2.py
+++ tasks/basics/test_12_understanding_functions_part2.py
@@ -4,90 +4,99 @@ from tasks.placeholders import *
 
 
 def demo(first, second=2, third=3):
+    """demo function"""
     return [first, second, third]
 
 # keyword arguments allows you to write one api without having a large number
 # of overloads for various scenarios.
 # add extra arguments where necessary.
 def test_function_call_with_keyword_arguments():
-    assert [__] == demo(10)
-    assert [__] == demo(10, 20)
-    assert [__] == demo(10, 20, 30)
-    assert [__] == demo(second=20)
-    assert [__] == demo(second=20, third=30)
-    assert [__] == demo(first=10, third=30)
-    assert [__] == demo(10, third=30)
+    """function call with keyword arguements"""
+    assert [10, 2, 3] == demo(10)
+    assert [10, 20, 3] == demo(10, 20)
+    assert [10, 20, 30] == demo(10, 20, 30)
+    assert [10, 20, 3] == demo(10, second=20)
+    assert [10, 20, 30] == demo(10, second=20, third=30)
+    assert [10, 2, 30] == demo(first=10, third=30)
+    assert [10, 2, 30] == demo(10, third=30)
 
 
 def demo_variable_args(first, *args):
+    """variable arguements function"""
     return args
 
 
 def my_merge(separator, *args):
+    """merge arguments fuction"""
     return separator.join(args)
 
-
 def test_function_with_variable_args():
+    """function with variable arguments"""
     result = demo_variable_args("hello", "world")
-    assert __ == type(result).__name__ #this is the type of args
-    assert (__) == result              #this is the value of args
+    assert 'tuple' == type(result).__name__ #this is the type of args
+    assert ("world",) == result              #this is the value of args
 
-    assert (__) == demo_variable_args("hello", 1, 2, 3)
+    assert (1, 2, 3) == demo_variable_args("hello", 1, 2, 3)
 
-    assert __ == my_merge(".", "one", "two", "three")
-    assert __ == my_merge(",", "one", "two", "three")
+    assert "one.two.three" == my_merge(".", "one", "two", "three")
+    assert "one,two,three" == my_merge(",", "one", "two", "three")
 
 
 def demo_with_keyword_args(name, *args, **kwargs):
+    """keyword arguments"""
     return kwargs
 
 
 def test_function_with_keyword_args():
+    """function with keyword arguments"""
     result = demo_with_keyword_args("jack", age=10, height=100)
-    assert __ == type(result).__name__
-    assert {__} == result
-    assert {__} == demo_with_keyword_args("jack", "address", age=10, height=100)
-    assert {__} == demo_with_keyword_args("jack", address="address", age=10, height=100)
-
+    assert 'dict' == type(result).__name__
+    assert {"age":10, "height":100} == result
+    assert {"age":10, "height":100} == demo_with_keyword_args("jack", "address", age=10, height=100)
+    assert {"address":"address", "age":10, "height":100} == demo_with_keyword_args("jack", address="address", age=10, height=100)
 
 def demo_sub(*args, **kwargs):
+    """demo function1"""
     return args, kwargs
 
 
 def demo_unpacking(name, *args, **kwargs):
+    """demo function2"""
     return demo_sub(*args, **kwargs)
 
 
 def demo_no_unpacking(name, *args, **kwargs):
+    """demo function3"""
     return demo_sub(args, kwargs)
 
 
 def test_function_unpacking():
+    """unpacking function"""
     result = demo_unpacking("jack", 1, 2, k1="v1", k2="v2")
-    assert (__) == result
+    assert ((1, 2), {'k1': 'v1', 'k2': 'v2'}) == result
 
     result = demo_no_unpacking("jack", 1, 2, k1="v1", k2="v2")
-    assert (__) == result
+    assert (((1, 2), {'k1': 'v1', 'k2': 'v2'}), {}) == result
 
     result = demo_sub(1,2, k1="v1")
-    assert (__) == result
+    assert ((1, 2), {"k1":"v1"}) == result
 
-    result = demo_sub((1,2), {"k1" :"v1"})
-    assert (__) == result
+    result = demo_sub((1,2), {"k1":"v1"})
+    assert (((1,2), {"k1":"v1"}), {}) == result
 
     result = demo_sub(*(1,2), **{"k1": "v1"})
-    assert (__) == result
+    assert ((1, 2), {"k1":"v1"}) == result
 
     #you can unpack lists as well
     result = demo_sub(*[1,2], **{"k1":"v1"})
-    assert (__) == result
+    assert ((1, 2), {"k1":"v1"}) == result
 
 
 
 three_things_i_learnt = """
--
--
--
+- arguments (return tuple) & keyword arguments (return dictionary) 
+- args have only name while kwargs have name and default value
+- kwargs should be placed after args; also packing and unpacking of args and kwargs affects what is returned
 """
 
 time_taken_minutes = ___
