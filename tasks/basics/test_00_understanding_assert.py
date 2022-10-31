5a66b38ca2ddd8423dd4cbabcf67b2414a58326f
Nikhil-samudrala <114351312+Nikhil-samudrala@users.noreply.github.com>
2022-10-31 13:15:05 +0530

    Pylint changes

--- tasks/basics/test_00_understanding_assert.py
+++ tasks/basics/test_00_understanding_assert.py
@@ -1,8 +1,10 @@
+""" Exercise 00 Basics of Pthon and about Assert """
+
 __author__ = 'Hari'
 
-from tasks.placeholders import *
+# from tasks.placeholders import *
 
-notes = '''
+NOTES = '''
 This lesson introduces the basic assert statement in python. assert is generally used to 'assert' the truth of an
 expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is raised with
 the <optional message>. If is evaluates to True, nothing happens.
@@ -12,33 +14,45 @@ the <optional message>. If is evaluates to True, nothing happens.
 
 
 def test_assert_true():
-    #throws assertion error
-    assert True  #This should be True -- replace ___ with True.
+    """ About Assert"""
+    # throws assertion error
+    assert True   # This should be True -- replace ___ with True.
+
 
 def test_assert_true_with_message():
-    assert True, "This is the failure message" # replace ___ with True to stop seeing the assertion error
+    """ Assert with Error Message"""
+    assert True, "This is the failure message"  # replace ___ with True to stop seeing the assertion error
+
 
 def test_assert_equality():
-    assert 7 == 2 + 5   #replace __ with the expected value
+    """ Arithematic Operation + """
+    assert 7 == 2 + 5   # replace __ with the expected value
 
-#Fill in __ in the statements below to make the asserts succeed
+
+# Fill in __ in the statements below to make the asserts succeed
 def test_make_assert_true_1():
-    assert 8>7, "Fill in a value greater than 7"
+    """ Arithematic Operation >"""
+    assert 8 > 7, "Fill in a value greater than 7"
 
-#you can use the interpreter to find the value of 2**30
+
+# you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
+    """ Arithematic Operation ** """
     assert 2**43 > 2**30, "Fill in value greater than 2**30"
 
-def test_make_assert_true_3():
-    s1 = "Hello, World"
-    s2 = "Hello, World"
-    assert s1 == s2
 
-three_things_i_learnt = """
+def test_make_assert_true_3():
+    """ Using == operator """
+    s_1 = "Hello, World"
+    s_2 = "Hello, World"
+    assert s_1 == s_2
+
+
+THREE_THINGS_I_LEARNT = """
 assert evaluates the expression and results in a boolean value
 If the expression valued as True it does nothing, else throws an error with the Message which we pass after the expression seperated by comma(,)
 Here in expression we can evaluate as complicated as we can until it evaluates as a boolean value.
 """
 
-time_taken_minutes = 1
+TIME_TAKEN_IN_MINUTES = 1
 
