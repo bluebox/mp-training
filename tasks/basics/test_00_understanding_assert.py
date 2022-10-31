76530b90eb619a42cbf8033aa4f5ae238cdc23e2
dev-deen <80711481+dev-deen@users.noreply.github.com>
2022-10-31 16:38:17 +0530

    Update test_00_understanding_assert.py

--- tasks/basics/test_00_understanding_assert.py
+++ tasks/basics/test_00_understanding_assert.py
@@ -1,45 +1,59 @@
+"""MODULE TO ILLUSTRATE FUNCTIONALITY OF ASSERT"""
+
 __author__ = 'Hari'
 
 from tasks.placeholders import *
 
-notes = '''
+NOTES = '''
 This lesson introduces the basic assert statement in python. assert is generally used to 'assert' the truth of an
-expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is raised with
+expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is 
+raised with
 the <optional message>. If is evaluates to True, nothing happens.
-
  In the tests below, replace the blanks with values so that the resulting expression is True.
 '''
 
 
 def test_assert_true():
-    #throws assertion error
-    assert True  #This should be True -- replace ___ with True.
+    """Test assert true"""
+    # throws assertion error
+    assert True  # This should be True -- replace ___ with True.
+
 
 def test_assert_true_with_message():
-    assert True, "This is the failure message" # replace ___ with True to stop seeing the assertion error
+    """test assert true with message"""
+    # replace ___ with True to stop seeing the assertion error
+    assert True, "This is the failure message"
+
 
 def test_assert_equality():
-    assert 7 == 2 + 5   #replace __ with the expected value
+    """test assert equality"""
+    assert 7 == 2 + 5   # replace __ with the expected value
 
-#Fill in __ in the statements below to make the asserts succeed
+
+# Fill in __ in the statements below to make the asserts succeed
 def test_make_assert_true_1():
-    assert 8>7, "Fill in a value greater than 7"
+    """test make assert true 1"""
+    assert 8 >= 7, "Fill in a value greater than 7"
 
-#you can use the interpreter to find the value of 2**30
+
+# you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
-    assert 2**43 > 2**30, "Fill in value greater than 2**30"
+    """Test make assert true 2"""
+    assert 2 ** 43 > 2 ** 30, "Fill in value greater than 2**30"
+
 
 def test_make_assert_true_3():
-    s1 = "Hello, World"
-    s2 = "Hello, World"
-    assert s1 == s2
+    """test make assert true 3"""
+    s1_ = "Hello, World"
+    s2_ = "Hello, World"
+    assert s1_ == s2_
 
-three_things_i_learnt = """
-asseret ==bool
+
+THREE_THINGS_I_LEARNT = """
+assert ==bool
 -assert raises AssertException if "condition followed by assert statement is false"
 -string followed by assert condition is: "AssertException statement"
 -assert can be used to test output of a python module
 """
 
-time_taken_minutes = 1
-
+TIME_TAKEN_MINUTES = 1
