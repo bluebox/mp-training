e81f121182bfa16ccdbaf4fc7f6651a149356aea
dev-deen <80711481+dev-deen@users.noreply.github.com>
2022-10-31 18:09:31 +0530

    Update test_01_understanding_numbers.py

--- tasks/basics/test_01_understanding_numbers.py
+++ tasks/basics/test_01_understanding_numbers.py
@@ -1,3 +1,5 @@
+"""MODULE TO ILLUSTRATE FUNCTIONALITY OF ASSERT"""
+
 __author__ = 'Hari'
 
 from tasks.placeholders import *
@@ -7,16 +9,20 @@ from tasks.placeholders import *
 # type(object) -> returns the object's type.
 
 def test_numbers_types():
-    assert 'int' == type(1).__name__
-    assert 'float' == type(1.3).__name__
+    """test numbers types"""
+    assert True is isinstance(1, int)
+    assert True is isinstance(1.3, float)
 # assert 'int' == type(10L).__name__
 # In Python 3 the long datatype has been removed and all integer values are handled
 # by the Int class. The default size of Int will depend on your CPU architecture.
-# 32 bit systems the default datatype for integers will be 'Int32'->[-2147483648,2147483647]
-# 64 bit systems the default datatype for integers will be 'Int64'->[-9223372036854775808,9223372036854775807]
+# 32-bit systems the default datatype for integers will be
+# 'Int32'->[-2147483648,2147483647]
+# 64-bit systems the default datatype for integers will be
+# 'Int64'->[-9223372036854775808,9223372036854775807]
 
 
 def test_numbers_int_arithmetic_operations():
+    """test numbers int arithmetic operations"""
     assert 30 == 10 + 20
     assert 200 == 10 * 20
     assert 32 == 2 ** 5
@@ -32,6 +38,7 @@ def test_numbers_string_to_int():
 
 
 def test_numbers_int_to_string():
+    """test numbers int to string"""
     assert '0o12' == oct(10)
     assert '0x64' == hex(100)
     assert '0b11111111' == bin(255)
@@ -42,11 +49,11 @@ def test_numbers_long():
     assert 1606938044258990275541962092341162602522202993782792835301376 == 2 ** 200
 
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 -we have a method in python to convert a decimal number to octal
-- a method to convert decimal to hexadecemal
+- a method to convert decimal to hexadecimal
 - a method to convert decimal to binary
 - hexadecimal, octal and binary function returns value as a string
 """
 
-time_taken_minutes = 4
+TIME_TAKEN_MINUTES = 4
