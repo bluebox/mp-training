9be9d2d2291097ce99ddff93bbcc8a62f51b757c
Abhishek MV <115617662+deadlock-mv@users.noreply.github.com>
2022-11-01 10:44:22 +0530

    pylinted

--- tasks/basics/test_00_understanding_assert.py
+++ tasks/basics/test_00_understanding_assert.py
@@ -1,45 +1,49 @@
+"""Introduction"""
+
 __author__ = 'Hari'
 
 from tasks.placeholders import *
 
-notes = '''
+NOTES = '''
 This lesson introduces the basic assert statement in python. assert is generally used to 'assert' the truth of an
 expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is raised with
 the <optional message>. If is evaluates to True, nothing happens.
-
  In the tests below, replace the blanks with values so that the resulting expression is True.
 '''
 
-
 def test_assert_true():
-    #throws assertion error
+    """throws assertion error"""
     assert True  #This should be True -- replace ___ with True.
 
 def test_assert_true_with_message():
-    assert True, "This is the failure message" # replace ___ with True to stop seeing the assertion error
+    """test make assert true"""
+    assert True, "This is the failure message"
 
 def test_assert_equality():
+    """test assert equality"""
     assert 7 == 2 + 5   #replace __ with the expected value
 
 #Fill in __ in the statements below to make the asserts succeed
 def test_make_assert_true_1():
+    """test make assert true"""
     assert 8>7, "Fill in a value greater than 7"
 
 #you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
+    """test make assert true"""
     assert 2**43 > 2**30, "Fill in value greater than 2**30"
 
 def test_make_assert_true_3():
-    s1 = "Hello, World"
-    s2 = "Hello, World"
-    assert s1 == s2
+    """test make assert true"""
+    s_1 = "Hello, World"
+    s_2 = "Hello, World"
+    assert s_1 == s_2
 
-three_things_i_learnt = """
+THREE_THING_I_LEARNT = """
 asseret ==bool
 -assert is used to know the truth of an expression
 -Alongside, a message can also be added to describe about the err
 -SYNTAX: assert <expression>, "<message">
 """
 
-time_taken_minutes = 1
-
+TIME_TAKEN_MINUTES = 1
