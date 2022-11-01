b35e54fd5f792c04c7a6f1134bd2ff99fee7f6cd
SoAbhious <106592121+SoAbhious@users.noreply.github.com>
2022-11-01 16:23:06 +0530

    Updated test_08_understanding_truth

--- tasks/basics/test_08_understanding_truth.py
+++ tasks/basics/test_08_understanding_truth.py
@@ -1,8 +1,9 @@
+"""understanding truth"""
+
 __author__ = 'Hari'
 
-from tasks.placeholders import *
 
-notes = '''
+NOTES = '''
 Just like C, python has notions on what values are considered true
 and what values are considered false.
 
@@ -13,12 +14,14 @@ with base values of the data types like 0, '', [] etc.
 
 #None is a first class object in python
 def test_none_type():
+    """none type"""
     assert 'NoneType' == type(None).__name__
 
 #In control flow, builtin objects like string, list, tuple have truth
 #and false values
 
 def test_truth_none():
+    """truth none"""
     value = None
     result = "not-set"
     #is None treated as true or false? =>flase(else loop)
@@ -31,18 +34,19 @@ def test_truth_none():
 
 # a helper function used to test the truth value of an object.
 def truth_test(object, description):
+    """truth test"""
     if object:
         return description + " is treated as true"
-    else:
-        return description + " is treated as false"
+    return description + " is treated as false"
 
 def test_truth_values():
-    assert is "empty string is treated as false" == truth_test("", "empty string")
+    """truth values"""
+    assert "empty string is treated as false" == truth_test("", "empty string")
     assert "empty tuple is treated as false" == truth_test((), "empty tuple")
     assert "empty list is treated as false" == truth_test([], "empty list")
     assert "empty dict is treated as false" == truth_test({}, "empty dict")
     assert "empty set is treated as false" == truth_test(set(), "empty set")
-    assert "white space is treated as false" == truth_test(" ", "white space")
+    assert "white space is treated as true" == truth_test(" ", "white space")
     assert "0 is treated as false" == truth_test(0, "0")
     assert "1 is treated as true" == truth_test(1, "1")
     assert "non-empty-string is treated as true" == truth_test("a", "non-empty-string")
@@ -54,27 +58,28 @@ def test_truth_values():
 # The fact that certain things are treated as True or False by
 # control flow statements does not mean that they are equal to True or False.
 def test_equality():
-    assert False == ("" == True)
-    assert False == (() == True)
-    assert False == ([] == True)
-    assert False == (set() == True)
-    assert False == (0 == True)
-    assert False == ("" == False)
-    assert False == (() == False)
-    assert False == ([] == False)
-    assert False == (set() == False)
-    assert True == (0 == False)
-    assert True == (1 == True)
-    assert False == ("a" == True)
-    assert False == ((1,2) == True)
-    assert False == ([1] == True)
-    assert False == ({1} == True)
+    """equality"""
+    assert False is ("" is True)
+    assert False is (() is True)
+    assert False is ([] is True)
+    assert False is (set() is True)
+    assert False is (0 is True)
+    assert False is ("" is False)
+    assert False is (() is False)
+    assert False is ([] is False)
+    assert False is (set() is False)
+    assert False is (0 is False)
+    assert False is (1 is True)
+    assert False is ("a" is True)
+    assert False is ((1, 2) is True)
+    assert False is ([1] is True)
+    assert False is ({1} is True)
 
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 - different values that can be treated as true or false
 - 'assert False == ("" == True)' these work differently than expected
 - undertood truth values
 """
 
-time_taken_minutes = ___
+TIME_TAKEN_MINUTES = 20
