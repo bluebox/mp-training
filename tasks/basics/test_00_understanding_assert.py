d8276a0d630b3ca8b560f381b1ebb8c16a64dab6
SoAbhious <106592121+SoAbhious@users.noreply.github.com>
2022-11-01 16:29:04 +0530

    Updated test_00_understanding_assert.

--- tasks/basics/test_00_understanding_assert.py
+++ tasks/basics/test_00_understanding_assert.py
@@ -1,8 +1,11 @@
+"""understanding assert"""
+
 __author__ = 'Hari'
 
-from tasks.placeholders import *
 
-notes = '''
+
+
+NOTES = '''
 This lesson introduces the basic assert statement in python. assert is generally used to 'assert' the truth of an
 expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is raised with
 the <optional message>. If is evaluates to True, nothing happens.
@@ -15,31 +18,40 @@ def test_assert_true():
     """thorows exception error"""
     assert True, "This should be True -- replace ___ with True."
 
+
 def test_assert_true_with_message():
     """assert with error message"""
-    assert True, "This is the failure message" 
+    assert True, "This is the failure message"
+
 
 def test_assert_equality():
+    """assert equality"""
     assert 7 == 2 + 5, "replace __ with the expected value"
 
-#Fill in __ in the statements below to make the asserts succeed
-def test_make_assert_true_1():
-    assert 8>7, "Fill in a value greater than 7"
 
-#you can use the interpreter to find the value of 2**30
+# Fill in __ in the statements below to make the asserts succeed
+def test_make_assert_true_1():
+    """make assert true 1"""
+    assert 8 > 7, "Fill in a value greater than 7"
+
+
+# you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
-    assert 2**43 > 2**30, "Fill in value greater than 2**30"
+    """make assert true 2"""
+    assert 2 ** 43 > 2 ** 30, "Fill in value greater than 2**30"
+
 
 def test_make_assert_true_3():
-    s1 = "Hello, World"
-    s2 = "Hello, World"
-    assert s1 == s2, "Not equal"
+    """make assert true 3"""
+    s_1 = "Hello, World"
+    s_2 = "Hello, World"
+    assert s_1 == s_2, "Not equal"
 
-three_things_i_learnt = """
+
+THREE_THINGS_I_LEARNT = """
 - assert == bool
 - how assert statement works
 - assertion failure message can also be added to the assert statement
 """
 
-time_taken_minutes = 1
-
+TIME_TAKEN_MINUTES = 1
