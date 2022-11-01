4c39aa2fa8e5274fd54624d442053012a9127b22
dev-deen <80711481+dev-deen@users.noreply.github.com>
2022-11-01 12:42:27 +0530

    Update test_12_understanding_functions_part2.py

--- tasks/basics/test_12_understanding_functions_part2.py
+++ tasks/basics/test_12_understanding_functions_part2.py
@@ -1,9 +1,12 @@
+"""MODULE-2 FOR UNDERSTANDING FUNCTIONS"""
+
 __author__ = 'Hari'
 
-from tasks.placeholders import *
+# from tasks.placeholders import *
 
 
 def demo(first, second=2, third=3):
+    """demo helper function"""
     return [first, second, third]
 
 # keyword arguments allows you to write one api without having a large number
@@ -12,6 +15,7 @@ def demo(first, second=2, third=3):
 
 
 def test_function_call_with_keyword_arguments():
+    """test function call with keyword arguments"""
     assert [10, 2, 3] == demo(10)
     assert [10, 20, 3] == demo(10, 20)
     assert [10, 20, 30] == demo(10, 20, 30)
@@ -22,14 +26,17 @@ def test_function_call_with_keyword_arguments():
 
 
 def demo_variable_args(first, *args):
+    """demo variable args"""
     return args
 
 
 def my_merge(separator, *args):
+    """my merge helper function"""
     return separator.join(args)
 
 
 def test_function_with_variable_args():
+    """test function with variable args"""
     result = demo_variable_args("hello", "world")
     assert 'tuple' == type(result).__name__  # this is the type of args
     assert ('world',) == result              # this is the value of args
@@ -41,10 +48,12 @@ def test_function_with_variable_args():
 
 
 def demo_with_keyword_args(name, *args, **kwargs):
+    """demo with keyword args"""
     return kwargs
 
 
 def test_function_with_keyword_args():
+    """test function with keyword args"""
     result = demo_with_keyword_args("jack", age=10, height=100)
     assert 'dict' == type(result).__name__
     assert {'age': 10, 'height': 100} == result
@@ -53,18 +62,22 @@ def test_function_with_keyword_args():
 
 
 def demo_sub(*args, **kwargs):
+    """demo sub"""
     return args, kwargs
 
 
 def demo_unpacking(name, *args, **kwargs):
+    """demo unpacking"""
     return demo_sub(*args, **kwargs)
 
 
 def demo_no_unpacking(name, *args, **kwargs):
+    """demo no unpacking"""
     return demo_sub(args, kwargs)
 
 
 def test_function_unpacking():
+    """test function unpacking"""
     result = demo_unpacking("jack", 1, 2, k1="v1", k2="v2")
     print(result)
     assert ((1, 2), {'k1': 'v1', 'k2': 'v2'}) == result
@@ -86,11 +99,10 @@ def test_function_unpacking():
     assert ((1, 2), {'k1': 'v1'}) == result
 
 
-
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 - keyword arguments allows you to write one api without having a large number of overloads for various scenarios. Add extra arguments where necessary.
 - We can have functions with variable arguments and we can also have variable keyword arguments and they are represented by * and ** beside them
 - The variable arguments are stored inside a tuple and the variable keyword are stored inside dict and we can easily pack and unpack them.
 """
 
-time_taken_minutes = 10
+TIME_TAKEN_MINUTES = 10
