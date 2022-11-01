03a20eea82cc8f97512f7df61eeb812ec43f2146
SoAbhious <106592121+SoAbhious@users.noreply.github.com>
2022-11-01 16:26:02 +0530

    Updated test_04_understanding_tuples

--- tasks/basics/test_04_understanding_tuples.py
+++ tasks/basics/test_04_understanding_tuples.py
@@ -1,26 +1,32 @@
+"""understanding tuples"""
+
 __author__ = 'Hari'
 
 from tasks.placeholders import *
 
-notes = '''
+NOTES = '''
 Tuples are yet another sequence type along the lines of strings and lists with
 its own characteristics.
 '''
 
 def test_tuple_type():
+    """tuple type"""
     test_tuple = (1,2)   # note the syntax
     assert 'tuple' == type(test_tuple).__name__
 
 def test_tuple_length():
+    """tuple length"""
     colors = ('red', 'blue', 'green')
     assert 3 == len(colors)
 
 def test_tuple_with_no_elements():
+    """tuple with no elements"""
     empty = ()
-    assert True == isinstance(empty, tuple)
+    assert True is isinstance(empty, tuple)
     assert 0 == len(empty)
 
 def test_tuple_with_one_element():
+    """tuple with one element"""
     test1 = (1)
     assert 'int' == type(test1).__name__
 
@@ -28,26 +34,30 @@ def test_tuple_with_one_element():
     assert 'tuple' == type(test2).__name__
 
 def test_tuple_can_be_indexed():
+    """tuple can be indexed"""
     colors = ('red', 'blue', 'green')
     assert 'red' == colors[0]
     assert 'blue' == colors[1]
     assert 'green' == colors[2]
 
 def test_tuple_can_be_sliced():
+    """tuple can be sliced"""
     colors = ('red', 'blue', 'green')
     assert ('blue', 'green') == colors[1:3]
     assert ('blue', ) == colors[1:2]  #remember the awkward syntax for single element tuples :)
 
 
 def test_tuples_are_immutable():
+    """tuples are immutable"""
     colors = ('red', 'blue', 'green')
     try:
         colors[0] = 'orange'
-    except TypeError as te:
-        print(te)#  print te # note the exception -> SyntaxError: Missing parentheses in call to 'print'.
+    except TypeError as t_e:
+        print(t_e)#  print te # note the exception -> SyntaxError: Missing parentheses in call to 'print'.
         assert True
 
 def test_tuples_can_be_nested():
+    """tuples can be nested"""
     top_left = (10,20)
     bottom_right = (40,50)
     rectangle = (top_left, bottom_right)
@@ -59,17 +69,18 @@ def test_tuples_can_be_nested():
 
 
 def test_tuple_unpacking():
+    """tuple unpacking"""
     pair = (10, 20)
-    a, b = pair
-    assert 10 == a
-    assert 20 == b
+    _a, _b = pair
+    assert 10 == _a
+    assert 20 == _b
 
     triplet = (10, 20, 30)
     try:
-        a, b = triplet
+        _a, _b = triplet
         # should not come here.
-    except ValueError as ve:
-        print (ve) # observe what is printed here. =>In Python 3, printing values changed from being a distinct statement to being an ordinary function call, so it now needs parentheses
+    except ValueError as v_e:
+        print (v_e) # observe what is printed here. =>In Python 3, printing values changed from being a distinct statement to being an ordinary function call, so it now needs parentheses
         assert  True  # ve=>too many values to unpack (expected 2)
 
 def test_sequence_conversion():
@@ -95,10 +106,10 @@ def test_sequence_conversion():
     word4 = "".join(list_1)
     assert "testing" == word4
 
-three_things_i_learnt = """
+THREE_THINGS_I_LEARNT = """
 - how list and tuples are different, and also tuple unpacking
 - weird way in which single element tuple is written/initialized
 - how tuple is converted to string
 """
 
-time_taken_minutes = __
+TIME_TAKEN_MINUTES = 10
