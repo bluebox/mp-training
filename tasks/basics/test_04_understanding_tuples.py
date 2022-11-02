851e8f0d02467d1d64038739b325558287ec0ad5
Nikhil-samudrala <114351312+Nikhil-samudrala@users.noreply.github.com>
2022-11-02 10:51:34 +0530

    Pylint Changes

--- tasks/basics/test_04_understanding_tuples.py
+++ tasks/basics/test_04_understanding_tuples.py
@@ -1,76 +1,94 @@
+""" understanding Tuples"""
+
 __author__ = 'Hari'
 
 from tasks.placeholders import *
 
-notes = '''
+NOTES = '''
 Tuples are yet another sequence type along the lines of strings and lists with
 its own characteristics.
 '''
 
 
 def test_tuple_type():
+    """ Tuple Type"""
     test_tuple = (1,2)   # note the syntax
     assert 'tuple' == type(test_tuple).__name__
 
 
 def test_tuple_length():
+    """ Tuple Length"""
     colors = ('red', 'blue', 'green')
     assert 3 == len(colors)
 
 
 def test_tuple_with_no_elements():
+    """ Empty Tuple"""
     empty = ()
     assert True == isinstance(empty, tuple)
     assert 0 == len(empty)
 
+    
 def test_tuple_with_one_element():
+    """ Tuple With one Element is Not Tuple unless Have a , """
     test1 = (1)
     assert 'int' == type(test1).__name__
 
     test2 = (1,)  #note the syntax used to disambiguate
     assert 'tuple' == type(test2).__name__
 
+    
 def test_tuple_can_be_indexed():
+    """ Tuples can be Indexed"""
     colors = ('red', 'blue', 'green')
     assert 'red' == colors[0]
     assert 'blue' == colors[1]
     assert 'green' == colors[2]
 
+    
 def test_tuple_can_be_sliced():
+    """ Tuples can be Sliced"""
     colors = ('red', 'blue', 'green')
     assert ('blue', 'green') == colors[1:3]
     assert ('blue',) == colors[1:2]  #remember the awkward syntax for single element tuples :)
 
+    
 def test_tuples_are_immutable():
+    """ Tuples are Immutable unlike Lists"""
     colors = ('red', 'blue', 'green')
     try:
         colors[0] = 'orange'
-    except TypeError as te:
+    except TypeError as t_e:
+        print(t_e) # Print t_e and note the exception
         assert True
 
+        
 def test_tuples_can_be_nested():
+    """ Nested Tuples """
     top_left = (10,20)
     bottom_right = (40,50)
     rectangle = (top_left, bottom_right)
 
     assert 2 == len(rectangle)
-    assert top_left == rectangle[0]
+    assert (10, 20) == rectangle[0]
     assert 10 == rectangle[0][0]
     assert 50 == rectangle[1][1]
 
+    
 def test_tuple_unpacking():
+    """ Tuple Unpacking"""
     pair = (10, 20)
-    a, b = pair
-    assert 10 == a
-    assert 20 == b
+    _a, _b = pair
+    assert 10 == _a
+    assert 20 == _b
 
     triplet = (10, 20, 30)
     try:
-        a, b = triplet
-        assert False # should not come here.
-    except ValueError as ve:
-        print (ve) # observe what is printed here. =>In Python 3, printing values changed from being a distinct statement to being an ordinary function call, so it now needs parentheses
-        assert  True  # ve=>too many values to unpack (expected 2)
+        _a, _b = triplet
+       # should not come here.
+    except ValueError as v_e:
+        print (v_e) # observe what is printed here. =>In Python 3, printing values changed from being a distinct statement to being an ordinary function call, so it now needs parentheses
+        assert True  # ve=>too many values to unpack (expected 2)
 
 
 def test_sequence_conversion():
@@ -96,11 +114,11 @@ def test_sequence_conversion():
     word4 = "".join(list_1)
     assert 'testing' == word4
     
-three_things_i_learnt = """
+    
+THREE_THINGS_I_LEARNT = """
 - Tuples like lists can be accessed using indexes and we can slice the tuples but unlike lists we can't alter the items inside tuples
 - Tuples with one element is not treated as a tuple until it is not suffixed with comma
 - Tuples can store tuples inside and can be accessed using indices and we can do tuple unpacking and can perform join operation using tuples
 """
 
-time_taken_minutes = 10
-
+TIME_TAKEN_MINUTES = 10
