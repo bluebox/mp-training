5c9f82fc611ac80761dc31a052805f2abf937e04
oraon-bipin <116059696+oraon-bipin@users.noreply.github.com>
2022-11-02 10:16:04 +0530

    Update test_01_understanding_numbers.py

--- tasks/basics/test_01_understanding_numbers.py
+++ tasks/basics/test_01_understanding_numbers.py
@@ -6,7 +6,9 @@ from tasks.placeholders import *
 # For most of these tests use the interpreter to fill up the blanks.
 # type(object) -> returns the object's type.
 
+""" Program to understand python number system"""
 def test_numbers_types():
+    """ function to check type of number"""
     assert 'int' == type(1).__name__
     assert 'float' == type(1.3).__name__
 # assert 'int' == type(10L).__name__
@@ -14,32 +16,35 @@ def test_numbers_types():
 # by the Int class. The default size of Int will depend on your CPU architecture.
 # 32 bit systems the default datatype for integers will be 'Int32'->[-2147483648,2147483647]
 # 64 bit systems the default datatype for integers will be 'Int64'->[-9223372036854775808,9223372036854775807]
-
+test_numbers_types()
 
 def test_numbers_int_arithmetic_operations():
+    """ function to check int number in python"""
     assert 30 == 10 + 20
     assert 200 == 10 * 20
     assert 32 == 2 ** 5
     assert -10 == 10 - 20
     assert 2.3333333333333335 == 7 / 3
-
+test_numbers_int_arithmetic_operations()
 
 def test_numbers_string_to_int():
     """hint: execute  print int.__doc__ in python console
        to find out what int(..) does"""
     assert 255 == int("FF", 16)
     assert 63 == int("77", 8)
-
+test_numbers_string_to_int()
 
 def test_numbers_int_to_string():
+    """ function to convert int to other type"""
     assert '0o12' == oct(10)
     assert '0x64' == hex(100)
     assert '0b11111111' == bin(255)
-
+test_numbers_int_to_string()
 
 def test_numbers_long():
     """Long is not the long in c"""
     assert 1606938044258990275541962092341162602522202993782792835301376 == 2 ** 200
+test_numbers_long()
 
 
 three_things_i_learnt = """
