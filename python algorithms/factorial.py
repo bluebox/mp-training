835191f95326db20b3834add1ad1a96e18980557
Mr-N-Patel <116059156+Mr-N-Patel@users.noreply.github.com>
2022-12-15 12:33:35 +0530

    Delete python algorithms directory

--- python algorithms/factorial.py
+++
@@ -1,7 +0,0 @@
-def factorial(num):
-    if(num == 1):
-        return 1
-    return factorial(num-1)*num
-
-n = int(input('Enter a number: '))
-print(factorial(n))
--- python algorithms/pattern.py
+++
@@ -1,5 +0,0 @@
-size = int(input('Enter size: '))
-for i in range(2*size-1):
-    for j in range(4*size-3):
-        if i%2 == 0:
-            print('-',end='')
