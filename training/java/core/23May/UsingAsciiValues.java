66bb08f13760db5221e9f8f23e573c77537a76d9
surya-j999 <surya.jalluri999@gmail.com>
2025-05-26 20:32:06 +0530

    Add files via upload

---
+++ training/java/core/23May/UsingAsciiValues.java
@@ -0,0 +1,9 @@
+public class UsingAsciiValues {
+	public static void main(String[] args) {
+		int AsciiOfOne = '1';
+		System.out.println("Ascii value of one : "+AsciiOfOne);
+		System.out.println("converted value of Ascii : " +(char) 68);
+		
+	}
+
+}
