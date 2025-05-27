40f425e9425867a4ac387d498716c0f37365a508
Manojreddy19 <manojreddykadipikonda2002@gmail.com>
2025-05-27 17:54:45 +0530

    Added ternary operator example

---
+++ training/java/core/Operators/src/differentoperators/TernaryOperator.java
@@ -0,0 +1,10 @@
+package differentoperators;
+
+public class TernaryOperator {
+	public static void main(String[] args)
+	{
+		int largerNumber = 5 > 7 ? 5 : 7;
+		System.out.println(largerNumber);
+	}
+
+}
