8bd1c2ce4b4914150d7a6f8481c2952a3ee54a59
revanth-g <82812901+revanth-g@users.noreply.github.com>
2025-05-27 20:19:36 +0530

    created

---
+++ training/java/core/MyPractice/src/samplecodes/SK.java
@@ -0,0 +1,7 @@
+package samplecodes;
+
+public class SK {
+	public static void main(String[] args) {
+		System.out.println("Dbjaispoe");
+	}
+}
---
+++ training/java/core/MyPractice/src/samplecodes/SmartKitchen.java
@@ -0,0 +1,67 @@
+package samplecodes;
+
+
+public class SmartKitchen {
+	private CoffeeMaker brewMaster;
+	private Refrigerator iceBox;
+	private DishWasher dishWash;
+	public SmartKitchen(CoffeeMaker brewMaster,Refrigerator iceBox,DishWasher dishWash) {
+		this.brewMaster=brewMaster;
+		this.iceBox=iceBox;
+		this.dishWash=dishWash;
+	}
+	public static void main(String[] args) {
+		CoffeeMaker cm=new CoffeeMaker();
+		Refrigerator rf= new Refrigerator();
+		DishWasher dw=new DishWasher();
+		SmartKitchen sk=new SmartKitchen(cm,rf,dw);
+		sk.setKitchenState(true, true, false);
+	}
+	public void addWater() {
+		brewMaster.hasWorkToDo=true;
+	}
+	public void pourMilk() {
+		iceBox.hasWorkToDo=true;
+	}
+	public void loadDishWasher() {
+		dishWash.hasWorkToDo=true;
+	}
+	public void setKitchenState(boolean refrigerator,boolean coffee,boolean dish) {
+		if(refrigerator) {
+			pourMilk();
+			iceBox.orderFood();
+		}
+		if(coffee) {
+			addWater();
+			brewMaster.brewCoffee();
+			
+		}
+		if(dish) {
+			loadDishWasher();
+			dishWash.doDishes();
+		}
+		
+	}
+}
+class Refrigerator{
+	boolean hasWorkToDo;
+	public void orderFood() {
+		System.out.println("Refrigerator - Food ordered");
+		hasWorkToDo=false;
+	}
+}
+class DishWasher{
+	boolean hasWorkToDo;
+	public void doDishes() {
+		System.out.println("DishWasher - Washing is done");
+		hasWorkToDo=false;
+	}
+}
+class CoffeeMaker{
+	boolean hasWorkToDo;
+	public void brewCoffee() {
+		System.out.println("CoffeeMaker - Coffee is Readyy");
+		hasWorkToDo=false;
+	}
+}
+
