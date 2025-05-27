886d3a00ec68d7bf91c8556b5f36a56859553062
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-05-27 18:59:12 +0530

    arithmetic operators done

---
+++ training/java/core/ArithmeticOperatorts.java
@@ -0,0 +1,20 @@
+public class Operatorts {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+
+        int sum = a+b;
+        int sub = a-b;
+        int mul = a * b;
+        int div = (int) a/b;
+        int mod = a%b;
+
+
+        System.out.println("sum : " + sum);
+        System.out.println("sub : " + sub);
+        System.out.println("mul : " + mul);
+        System.out.println("div : " + div);
+        System.out.println("mod : " + mod);
+        
+    }
+}
