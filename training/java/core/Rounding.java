00ad022eec5162505c67a7a90620ea664018869e
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-05-27 17:18:51 +0530

    rouding completed

---
+++ training/java/core/Rounding.java
@@ -0,0 +1,9 @@
+
+public class Rounding{
+    public static void main(String[] args){
+
+        
+        System.out.println(Math.round(3.4));
+        System.out.println(Math.round(3.5));
+    }
+}
