55fc444f6b2c1082841fcae2ca53681ecb65fb94
Manojreddy19 <manojreddykadipikonda2002@gmail.com>
2025-05-27 17:36:58 +0530

    added arithematic operators

---
+++ training/java/core/PerfectNumber.java
@@ -0,0 +1,10 @@
+public class PerfectNumber{
+    public static void main(String args[])
+    {
+        isPerfect(6);
+    }
+    public static isPerfect(int num)
+    {
+        System.out.println("test");
+    }
+}
