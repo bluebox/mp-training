be83bf1198b3bc0ca0f74da90a9f877c2d9179f0
Manojreddy19 <manojreddykadipikonda2002@gmail.com>
2025-05-27 17:50:25 +0530

    Added logical operators

---
+++ training/java/core/Operators/src/differentoperators/LogicalOperators.java
@@ -0,0 +1,21 @@
+package differentoperators;
+
+public class LogicalOperators {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		
+		System.out.println(a && b);
+		System.out.println(a || b);
+		System.out.println((!a));
+		
+		int c = 5;
+		int d = 7;
+		System.out.println(c > d);
+		System.out.println(c < d);
+		System.out.println(c == d);
+		System.out.println(c >= d);
+		
+	}
+
+}
