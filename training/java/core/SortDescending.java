1a82613b0ec4c9c9cf951d24374d06aae0b734a3
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-05-27 19:25:12 +0530

    descending order sorting done

---
+++ training/java/core/SortDescending.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SortDescending {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,36,9,4,5};
+
+        
+
+        System.out.println(Arrays.toString(arr));
+
+        Arrays.sort(arr);
+
+        int st = 0;
+        int end = arr.length - 1;
+
+        //System.out.println(Arrays.toString(arr));
+
+        while (st < end){
+            int temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+
+            st++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+      
+    }
+}
