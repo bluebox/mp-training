235470c60fc5c01414738ad47a00ea6b10a4d017
abhinavreddy-ch <abhinavreddychinni3@gmail.com>
2025-05-27 19:45:43 +0530

    created practice
    basics of conditional logics  and reusablity of code

---
+++ practice/AnnalynsInfiltrationProject/src/game/AnnalynsInfiltration.java
@@ -0,0 +1,19 @@
+package game;
+
+public class AnnalynsInfiltration {
+
+	public static boolean canFastAttack(boolean knightIsAwake) {
+		return !knightIsAwake;
+	}
+	public static boolean canSpy(boolean knightIsAwake,boolean archerIsAwake,boolean prisonerIsAwake){
+		return (!canFastAttack(knightIsAwake)|| canSignalPrisoner( archerIsAwake,prisonerIsAwake));
+	}
+	public static boolean canSignalPrisoner(boolean archerIsAwake,boolean prisonerIsAwake) {
+		return (archerIsAwake || prisonerIsAwake);
+	}
+	public static boolean canFreePrisoner(boolean knightIsAwake,boolean archerIsAwake,
+			boolean prisonerIsAwake,boolean petDogIsPresent) {
+		return (knightIsAwake && archerIsAwake &&prisonerIsAwake && petDogIsPresent);
+	}
+	
+}
---
+++ practice/AnnalynsInfiltrationProject/src/game/Main.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnnalynsInfiltration anna = new AnnalynsInfiltration();
+		anna.canFastAttack(false);
+		anna.canFreePrisoner(true, true, false, false);
+	}
+
+}
