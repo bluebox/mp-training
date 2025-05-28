0180f155fbd3f5a9b68dfa22c7e2ae493360c131
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-05-28 09:24:31 +0530

    control statements done

---
+++ training/java/core/Control_Statements/Control_Statements.java
@@ -0,0 +1,19 @@
+package Control_Statements;
+
+public class Control_Statements {
+    public static void main(String[] args) {
+        int a = 10 , b = 65;
+        String s1 = "naga";
+        String s2 = "reddy";
+
+        if(a > b){
+            System.out.println("a is greater than b");
+        }
+        else if(a ==b){
+            System.out.println("a equals b");
+        }
+        else{
+            System.out.println("a less than b");
+        }
+    }
+}
