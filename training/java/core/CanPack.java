3227e78a8a8da26a69d9e1885c362c048424070f
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-05-29 17:27:09 +0530

    can pack done

---
+++ training/java/core/CanPack.java
@@ -0,0 +1,40 @@
+public class CanPack {
+
+    public static void canPack(int a,int b,int c){
+        int w = (int) c / 5;
+        int rem = c%5;
+        if(a < 0 || b< 0 || c<0){
+            System.out.println("false");
+            return;
+        }
+
+        if (w == c){
+            System.out.println("true");
+            return;
+        }
+        else if(w > c){
+            w = w - c;
+        }
+        else if(w < c){
+            w = 0;
+        }
+
+        if(w + rem <= b){
+            System.out.println("true");
+            return;
+        }
+        else{
+            System.out.println("false");
+
+        }
+
+        
+    }
+    public static void main(String[] args) {
+        canPack(1, 0, 4);
+        canPack(1, 0, 5);
+        canPack(0, 5, 4);
+        canPack(2, 2, 11);
+        canPack(-3, 2, 12);
+    }
+}
