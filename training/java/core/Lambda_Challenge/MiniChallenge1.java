2e50a8fe7443c43630380fa5ca0984c8d14a516d
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-05-29 20:16:09 +0530

    challenge 1 done

---
+++ training/java/core/Lambda_Challenge/MiniChallenge1.java
@@ -0,0 +1,13 @@
+package Lambda_Challenge;
+import java.util.Arrays;
+import java.util.List;
+
+public class MiniChallenge1 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("naga","bhushan","saketh");
+
+    list.forEach(System.out::println);
+    }
+    
+
+}
