a3bb40a4b8e9bfd6a9c7e6325450165c3efd2362
Surya Jalluri <surya.jalluri999@gmail.com>
2025-05-30 18:48:00 +0530

    inner class (public static)

---
+++ training/java/core/Mastering Abstraction &amp; Interfaces: Expand Polymorphism & Java Hierarchies/src/playlistUsingInnerClass/Song.java
@@ -0,0 +1,19 @@
+package playlistUsingInnerClass;
+
+public class Song {
+ String title;
+ double duration;
+public Song(String title, double duration) {
+	super();
+	this.title = title;
+	this.duration = duration;
+}
+public String getTitle() {
+	return title;
+}
+public String toString()
+{
+	return (""+title+": "+duration);
+}
+ 
+}
