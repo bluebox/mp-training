29f30642525d04cbeb298be5fd873243f9957c63
Manojreddy19 <manojreddykadipikonda2002@gmail.com>
2025-05-30 20:15:33 +0530

    Added  add Exception Handing in Gym management

--- tasks/GymManagementSystem/src/gym/Gym.java
+++ tasks/GymManagementSystem/src/gym/Gym.java
@@ -3,54 +3,44 @@ package gym;
 import java.util.HashMap;
 
 class Gym {
-	
-	HashMap<Long,Member> members;
-	static String name="MEDPLUS GYM";
-	static String description="Prevation is better than cure";
-	
-	
+
+	HashMap<Long, Member> members;
+	static String name = "MEDPLUS GYM";
+	static String description = "Prevation is better than cure";
+
 	public Gym() {
 		super();
-		members=new HashMap<>();
-		System.out.println("Welcome to "+name);
-		System.out.println("**** " +description);
-		}
-
-	
-	public void addMember(Member member)
-	{
-		members.put(member.getMemberId(),member);
-		
+		members = new HashMap<>();
+		System.out.println("Welcome to " + name);
+		System.out.println("**** " + description);
 	}
-	public void deleteUser(long id)
-	{
+
+	public void addMember(Member member) {
+		members.put(member.getMemberId(), member);
+
+	}
+
+	public void deleteUser(long id) {
 		members.remove(id);
 	}
-	public void printDetails()
-	{
-//		System.out.println("Welcome to "+name);
-//		System.out.println("**** " +description);
+
+	public void printDetails() {
 		System.out.println("-".repeat(50));
-		members.forEach((l,member)->{
+		members.forEach((l, member) -> {
 			System.out.println();
 			member.getDetails();
-			
-			
+
 		});
 	}
-	public Member getMember(long id)
-	{
-		if(members.containsKey(id))
-		{
+
+	public Member getMember(long id) {
+		if (members.containsKey(id)) {
 			return members.get(id);
-		}
-		else
-		{
-			
+		} else {
+
 			return null;
 		}
-		
-	}
 
+	}
 
 }
