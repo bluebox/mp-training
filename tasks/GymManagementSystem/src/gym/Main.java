b2c4f50a92ccde72346218f2de4aefa0e775d066
Manoj Reddy <manojreddykadipikonda2002@gmail.com>
2025-06-01 14:44:54 +0530

    Added new features in gym management syatem

--- tasks/GymManagementSystem/src/gym/Main.java
+++ tasks/GymManagementSystem/src/gym/Main.java
@@ -12,7 +12,9 @@ public class Main {
 		sampleData(gym);
 
 		do {
-			System.out.println("1.Create new user \n2.remove an user\n3.Print Details\n4.add Plan\n5.exit");
+			System.out.println();
+			System.out.println("1.Create new user \n2.Update user\n3.remove an user\n4"
+					+ ".search user \n5.Print Details\n6.add Plan\n7.exit");
 
 			System.out.println("Please select ");
 			String sch = sc.next();
@@ -20,10 +22,12 @@ public class Main {
 				int ch = Integer.valueOf(sch);
 				switch (ch) {
 				case 1 -> createUser(gym);
-				case 2 -> deleteUser(gym);
-				case 3 -> gym.printDetails();
-				case 4 -> addPlan(gym);
-				case 5 -> System.exit(0);
+				case 2-> updateUser(gym);
+				case 3 -> deleteUser(gym);
+				case 4->searchUser(gym);
+				case 5 -> gym.printDetails();
+				case 6 -> addPlan(gym);
+				case 7 -> System.exit(0);
 				default -> System.out.println("Invalid input please select from options");
 				}
 				;
@@ -32,7 +36,31 @@ public class Main {
 			}
 		} while (true);
 	}
-
+	public static void updateUser(Gym gym)
+	{
+		System.out.println("Enter the user id");
+		long id;
+		try {
+			id=sc.nextLong();
+			Member member=gym.getMember(id);
+			if(member==null)
+			{
+				System.out.println("User Not found");
+				return;
+			}
+			System.out.println("Enter new name");
+			member.setName(sc.next());
+			System.out.println("Enter new age");
+			member.setAge(sc.nextInt());
+			System.out.println("User updated succesfully");
+			
+		}catch(Exception e){
+			System.out.println("Enter correct details "+e);
+		}
+		
+		
+	}
+	
 	public static void createUser(Gym gym) {
 		System.out.println("Enter your Name");
 		String name = sc.next();
@@ -62,14 +90,31 @@ public class Main {
 
 	public static void deleteUser(Gym gym) {
 		System.out.println("Enter memberId:");
-		long id = sc.nextLong();
-		gym.deleteUser(id);
+		long id;
+		try {
+			id= sc.nextLong();
+			gym.deleteUser(id);
+			
+			
+		}catch(Exception e){
+			System.out.println("Please Enter id "+e);
+			
+		}
+		
 
 	}
 
 	public static void addPlan(Gym gym) {
 		System.out.println("Enter the Id of the user");
-		long id = sc.nextInt();
+		long id;
+		try {
+			id= sc.nextLong();
+			
+		}catch(Exception e){
+			System.out.println("Please Enter id "+e);
+			return;
+			
+		}
 		Member member = gym.getMember(id);
 		if (member == null) {
 			System.out.println("User Not found");
@@ -94,9 +139,22 @@ public class Main {
 		System.out.println("Enter the duration");
 		int duration = sc.nextInt();
 		member.addPlan(details.get(opiton - 1), duration);
+		System.out.println("Plan sucessfull added to the user with id "+member.getMemberId());
 		
 	}
-
+	public static void searchUser(Gym gym)
+	{
+		System.out.println("Enter the user id");
+		long id;
+		try {
+			id=sc.nextLong();
+			gym.getMember(id).getDetails();
+		}catch(Exception e)
+		{
+			System.out.println("Enter correct user id "+e);
+		}
+	}
+	
 	public static void sampleData(Gym gym) {
 		Member member = new Member("mani", 20);
 		member.addPlan(PlanDetails.BASIC, 9);
