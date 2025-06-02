678bde4fdbee3b3d0a2e6d2d18525af20ba1073e
Chaitanya-1104 <93830104+Chaitanya-1104@users.noreply.github.com>
2025-06-02 14:20:46 +0530

    Case Study submission

---
+++ training/java/core/Case_Study/GYM_Management_System/MemberShipPlan.java
@@ -0,0 +1,54 @@
+public class MemberShipPlan implements Display, Identity {
+
+    private String plan;
+    private int duration;
+    private double fee;
+
+    public MemberShipPlan(String name, int duration, double fee) {
+        this.plan = name;
+        this.duration = duration;
+        this.fee = fee;
+    }
+
+    public String getPlan() {
+        return plan;
+
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        if (duration <= 0) {
+            throw new IllegalArgumentException("Duration must be positive");
+        }
+        this.duration = duration;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        if (fee < 0) {
+            throw new IllegalArgumentException("Fee cannot be negative.");
+        }
+        this.fee = fee;
+    }
+
+    @Override
+    public String getId() {
+        return plan;
+    }
+
+    @Override
+    public void showDetails() {
+
+        System.out.println(plan + " " + duration + " " + fee);
+    }
+}
