9b9c9b678821236d1434fba08752445217d4ef2a
Krishna Prasad <pinnurisaikrishna@gmail.com>
2025-06-03 01:18:47 +0530

    The Gym UserCase using OOPS

---
+++ training/java/core/pack/gymProject/Member.java
@@ -0,0 +1,32 @@
+package gymProject;
+
+public class Member extends Person {
+    private String memberId;
+    private MembershipPlan plan=null;
+
+    public Member(String memberId, String name, int age) {
+        super(name, age);
+        this.memberId = memberId;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void assignPlan(MembershipPlan plan) {
+        this.plan = plan;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Member ID: " + memberId);
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        if (plan != null) {
+            plan.showDetails();
+        } else {
+            System.out.println("No plan assigned");
+        }
+        System.out.println("-------------------------");
+    }
+}
