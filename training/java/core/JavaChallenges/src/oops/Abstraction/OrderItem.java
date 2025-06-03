2a72f8971e05e4567c3cf5ca62f232d1f8bee71e
uday-9949 <puliudaykiran1234@gmail.com>
2025-06-03 13:26:58 +0530

    Added

---
+++ training/java/core/JavaChallenges/src/oops/Abstraction/OrderItem.java
@@ -0,0 +1,11 @@
+package oops.Abstraction;
+
+
+public class OrderItem {
+    public int qty;
+    public ProductForSale product;
+    public OrderItem(int qty,ProductForSale product) {
+   	 this.qty=qty;
+   	 this.product=product;
+    }
+}
