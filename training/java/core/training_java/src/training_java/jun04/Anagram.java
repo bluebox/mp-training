fd68755087b34409cde7d7e79f6702fb94bc827e
Maneesh Kandra <150435242+kandramanish@users.noreply.github.com>
2025-06-04 22:29:37 +0530

    Update Anagram.java

--- training/java/core/training_java/src/training_java/jun04/Anagram.java
+++ training/java/core/training_java/src/training_java/jun04/Anagram.java
@@ -1,5 +1,5 @@
-package FinalandStreams;
-
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -7,8 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-
-public class Anagram {
+class Main {
 
 	public static void main(String[] args) {
 		ArrayList<String> lis=new ArrayList<String>(List.of("care","race","acer","acre","spy","psy","game","mage","mega","Bike"));
@@ -18,8 +17,19 @@ public class Anagram {
 			char[] item=member.toCharArray();
 			Arrays.sort(item);
 			String s=new String(item);
-			set.add(s);		
+			if(anag.containsKey(s)){
+			    anag.get(s).add(member);
+			}
+			else{
+			    anag.put(s,new ArrayList<String>(List.of(member)));
+			}
 		}
+		ArrayList<ArrayList<String>>  anagrams=new ArrayList<ArrayList<String>>();
+		for(var key:anag.keySet()){
+		    anagrams.add(anag.get(key));
+		}
+		System.out.println(anag+" "+anagrams);
+		
 		
 		
 	}
