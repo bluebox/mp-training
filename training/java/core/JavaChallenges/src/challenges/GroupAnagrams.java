9d6ee19f08863fccf6a60bcdccac240f7f36c828
uday-9949 <puliudaykiran1234@gmail.com>
2025-06-05 19:39:02 +0530

    Added

---
+++ training/java/core/JavaChallenges/src/challenges/GroupAnagrams.java
@@ -0,0 +1,37 @@
+package challenges;
+
+import java.util.*;
+public class GroupAnagrams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> l=new ArrayList<>();
+		l.add("abc");
+		l.add("cba");
+		l.add("b");
+		l.add("bb");
+		l.add("uday");
+		l.add("dayu");
+		HashMap<String,List<String>> hm=new HashMap<>();
+		for(String s:l)
+		{
+			char ch[]=s.toCharArray();
+			Arrays.sort(ch);
+			hm.getOrDefault(String.valueOf(ch),new ArrayList<>()).add(s);
+//			hm.putIfAbsent(String.valueOf(ch),new ArrayList<>());
+//			hm.get(String.valueOf(ch)).add(s);
+		}
+
+
+		List<List<String>> res=new ArrayList<>();
+		for(List<String> v:hm.values())
+		{
+			res.add(v);
+		}
+		System.out.println(res);
+
+	}
+	
+	
+
+}
