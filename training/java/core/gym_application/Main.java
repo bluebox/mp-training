9495dede16cdf189695cac6ac0b6cf81d4b49d8e
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-06-05 09:12:52 +0530

    pulled

--- training/java/core/gym_application/Main.java
+++ training/java/core/gym_application/Main.java
@@ -1,10 +1,163 @@
-package gymapp;
+package gym_application;
 import java.util.ArrayList;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.HashMap;
+
+interface Gym{
+
+    void gymMemberDetails();
+
+    void gymMembershipPlans();
+
+    void gymTrainerDetails();
+
+}
+
+
+class MyGym implements Gym{
+
+    private String gym_name = "beFit gym";
+    private String gymLocation = "Hyderabad";
+    private String manager_name = "naga";
+    private String managerId = "befit10001";
+    private String managerPassword = "naga";
+    static ArrayList<Member> members;
+    static ArrayList<Trainer> trainers;
+
+    MyGym(){
+        members = new ArrayList<>();
+        trainers = new ArrayList<>();
+    }
+
+    MyGym(String managerId , String managerPassword){
+        this.managerId = managerId;
+        this.managerPassword = managerPassword;
+        members = new ArrayList<>();
+        trainers = new ArrayList<>();
+    }
+
+    public String getManagerName(){
+        return this.manager_name;
+    }
+    public String getManagerPassword(){
+        return this.managerPassword;
+    }
+
+    public void addMember(Member obj){
+        members.add(obj);
+    }
+
+    public void removeMember(Member obj){
+        if( members.remove(obj) ) Member.members_count--;
+    }
+
+    public void addTrainer(Trainer obj){
+        trainers.add(obj);
+    }
+
+    public void removeTrainer(Trainer obj){
+        trainers.remove(obj);
+    }
+
+    public void gymDetails(){
+        System.out.println("-".repeat(20));
+        System.out.println("GYM DETAILS");
+        System.out.println("Gym name : "+ this.gym_name + " location : " + this.gymLocation);
+    }
+
+    public void gymManagerDetails(){
+        System.out.println("-".repeat(20));
+        System.out.println("GYM MANAGER DETAILS");
+        System.out.println("Manager name : "+this.manager_name + " manager id : "+ this.managerId);
+    }
+
+    public void gymMemberDetails(){
+        System.out.println("-".repeat(20));
+        System.out.println("GYM MEMBERS DETAILS");
+        for(Member mem : members){
+            System.out.println(mem.showMemberDetails());
+        }
+    }
+
+    public void gymMemberNames(){
+        System.out.println("-".repeat(20));
+        System.out.println("GYM MEMBERS NAMES");
+        for(Member mem : members){
+            System.out.println(mem.name);
+        }
+    }
+
+
+    public static Trainer getTrainerByName(String name){
+        
+        for(Trainer obj: trainers){
+            if(obj.name.equalsIgnoreCase(name)){
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    public static Trainer getTrainerById(String id){
+        
+        for(Trainer obj: trainers){
+            if(obj.trainerId.equals(id)){
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    public static Member getMemberByName(String name){
+        
+        for(Member obj: members){
+            if(obj.name.equalsIgnoreCase(name)){
+                return obj;
+                
+            }
+        }
+        return null;
+    }
+
+    public static Member getMemberById(String id){
+        
+        for(Member obj: members){
+            if(obj.getMemberId().equals(id)){
+                return obj;              
+            }
+        }
+        return null;
+    }
+    
+    public void gymTrainerDetails(){
+        System.out.println("-".repeat(20));
+        System.out.println("GYM TRAINERS :");
+        for(Trainer trainer : trainers){
+            trainer.details();
+        }
+    }
+
+    public void gymTrainerNames(){
+        System.out.println("-".repeat(20));
+        System.out.println("GYM TRAINERS :");
+        int i = 1;
+        for(Trainer trainer : trainers){
+           System.out.println(i +": " +trainer.name);
+           i++;
+        }
+    }
+
+    public String getTrainerByIndex(int idx){
+       return trainers.get(idx - 1).name;    
+    }
+    
+    public void gymMembershipPlans(){
+        MembershipPlan.showPlans();
+    }
+}
 
 
 public class Main {
@@ -135,7 +288,7 @@ public class Main {
 
             System.out.println("--------------------------------------------------------------------------");
             System.out.println("Welcome to Befit Gym");
-            System.out.println("1.Admin    2.Existing User    3.New User Registration   4.Trainer   q.Quit");
+            System.out.println("1.Admin    2.Existing Member    3.New Member Registration   4.Trainer   q.Quit");
 
             String ch = sc.nextLine();
 
