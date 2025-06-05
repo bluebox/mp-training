5244c13af505d98c689cc3df8b2b4aca21a8b39e
Manojreddy19 <manojreddykadipikonda2002@gmail.com>
2025-06-05 17:50:09 +0530

    Date and time

---
+++ training/java/core/MathUsage/src/math/Main.java
@@ -0,0 +1,18 @@
+package math;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(Math.abs(Integer.MIN_VALUE));
+//		System.out.println(Math.absExact(Integer.MIN_VALUE));
+
+		System.out.println(Math.abs(-4.56));
+		System.out.println("2power10 "+Math.pow(2,10));
+		System.out.println(Math.ceil(-4.56));
+		System.out.println(Math.floor(4.56));
+		System.out.println(Math.ceil(4.0001));
+		System.out.println(Math.sqrt(25));
+		System.out.println(Math.round(-4.56));
+		
+	}
+
+}
