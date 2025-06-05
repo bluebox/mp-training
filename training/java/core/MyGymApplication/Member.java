c67a820b86923caed9a3c722baea8c3c626a2bbc
kattamuriNagabhushan <nbhushan52@gmail.com>
2025-06-05 13:13:57 +0530

    moved myapplicationgym

---
+++ training/java/core/MyGymApplication/Member.java
@@ -0,0 +1,88 @@
+package MyGymApplication;
+import java.time.LocalDate;
+public class Member extends Person {
+
+    private String name;
+    private int age;
+    private String planTaken;
+    private LocalDate joiningDate;
+    private LocalDate endingDate;
+    private String memberId;
+    private String trainerName;
+
+    static int membersCount = 10001;
+
+    Member(){
+        this("" );
+    }
+
+    Member(String name){
+        this(name , -1 , "" , null);
+    }
+
+    Member(String name , int age ){
+        this(name , age , "" , null);
+    }
+
+    Member(String name , int age , String planTaken ){
+       this(name, age , planTaken , null);
+    }
+
+    Member(String name , int age , String planTaken , Trainer trainer){
+        this.name = name;
+        this.age = age;
+        this.joiningDate = LocalDate.now();
+        if(!planTaken.isEmpty())
+            this.planTaken = planTaken;
+            switch (planTaken) {
+                case "basic" -> this.endingDate = this.joiningDate.plusMonths(1);
+                case "gold" -> this.endingDate = this.joiningDate.plusDays(6);
+                case "premium" -> this.endingDate = this.joiningDate.plusYears(1);
+            }
+
+        
+        if(trainer!=null){
+            trainer.addTrainee(this.name);
+            this.trainerName = trainer.getName();
+        }
+         
+
+        
+        this.membersCount+=1;
+        this.memberId = "befit" + this.membersCount;
+    }
+
+    public String getMemberId(){
+        return this.memberId;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+
+    public String showMemberPlan(){
+        return "Plan : " + planTaken + "";
+    }
+
+    public boolean isActive(){
+
+        return true;
+    }
+
+    public String showMemberDetails(){
+        String s = "Name : " + this.name + ",  age : " + this.age + 
+        ",  id : " +  
+        this. memberId+",  plan taken : "+ this.planTaken + 
+        "\nDOJ : "+ this.joiningDate + ",  ending date : " + this.endingDate ;
+
+        if (trainerName!=null && trainerName!=""){
+
+         s = s + ",  trainer name : " + trainerName;
+        }
+
+        return s + "\n----------------------------------------------------------------------";
+
+    }
+
+}
---
+++ training/java/core/MyGymApplication/MembershipPlan.java
@@ -0,0 +1,9 @@
+package MyGymApplication;
+
+public class MembershipPlan{
+    public static void showPlans(){
+        System.out.println("1. Basic Plan -> cost : 1000 rs per month \n2. Gold Plan -> " +
+        "800 per month 6 months agreement (total : 4200 rs)\n3. Premium Plan 700 per month 1 year agreement (total : 8400 rs)");
+    }
+    
+}
