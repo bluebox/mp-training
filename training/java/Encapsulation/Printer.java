f39d818ec3a1841d882546ce3df8ef053e038a30
k-Rakeshram <rakeshram0179@gmail.com>
2025-06-06 14:05:44 +0530

    new files

---
+++ training/java/Encapsulation/Printer.java
@@ -0,0 +1,55 @@
+package Encapsulation;
+
+public class Printer {
+	private int tonerLevel;
+	private int pagesPrinted=0;
+	private boolean duplex;
+	
+	public Printer(int level,boolean plex) {
+		this.tonerLevel=level;
+		this.duplex=plex;
+	}
+	
+	public int addToner(int tonerAmount) {
+		if(tonerLevel+tonerAmount >0 && tonerLevel+tonerAmount<100) {
+			this.tonerLevel=tonerLevel+tonerAmount;
+			return tonerLevel;
+		}
+		return -1;
+	}
+	
+	public int printPages(int pages) {
+		if(duplex) {
+			pagesPrinted+=(pages*2);
+			System.out.println("It is a duplex printer");
+		}
+		else {
+			pagesPrinted+=pages;
+		}
+		return pagesPrinted;
+	}
+	
+	public int getTonerLevel(){
+	return tonerLevel;
+	}
+	public int getPagesPrinted(){
+	return pagesPrinted;
+	}
+	public void removeDuplex() {
+		this.duplex=false;
+	}
+	public void setDuplex() {
+		this.duplex=true;
+	}
+	
+	public static void main(String[] args) {
+		Printer p=new Printer(1,false);
+		p.removeDuplex();
+		System.out.println("Pages are :"+p.printPages(3));
+		System.out.println("Present Toner Level is :"+p.addToner(3));
+		p.setDuplex();
+		System.out.println("Pages printed are :"+p.getPagesPrinted(5));
+		System.out.println("Present Toner Level is :"+p.addToner(30));
+		
+	}
+}
