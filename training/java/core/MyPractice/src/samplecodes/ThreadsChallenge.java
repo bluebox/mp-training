8c09bd9f2fa44169aafb608e47198eda176a846c
revanth-g <82812901+revanth-g@users.noreply.github.com>
2025-06-06 19:52:49 +0530

    Solved challenges on 6th june

---
+++ training/java/core/MyPractice/src/samplecodes/ThreadsChallenge.java
@@ -0,0 +1,44 @@
+package samplecodes;
+
+public class ThreadsChallenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EvenNumbers evenNumbers=new EvenNumbers();
+		OddNumbers on=new OddNumbers();
+		Thread oddNumbers=new Thread(on);
+		oddNumbers.start();
+		evenNumbers.start();
+	}
+
+}
+class EvenNumbers extends Thread{
+	@Override
+	public void run() {
+		for(int i=2;i<=10;i+=2) {
+			System.out.print(i+" ");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+		}
+	}
+}
+class OddNumbers implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i=1;i<=10;i+=2) {
+			System.out.print(i+" ");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
