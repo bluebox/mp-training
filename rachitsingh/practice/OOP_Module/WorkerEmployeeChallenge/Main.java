5cdf337da5108b2e32da3c4764915549ca3faab9
Medplus Developer <dev@medplus.com>
2025-06-06 19:00:28 +0530

    Added WorkerEmployeeChallenge package

---
+++ rachitsingh/practice/OOP_Module/WorkerEmployeeChallenge/Main.java
@@ -0,0 +1,13 @@
+package WorkerEmployeeChallenge;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		SalariedEmployee emp = new SalariedEmployee("Mukesh Sharma", "1989", 891234, "2018-04-21", 2200000);
+		System.out.println("Employee Name: " + emp.getName());
+		System.out.println(emp.getName() + " is " + emp.getAge() + " years old.");
+		System.out.println("Monthly salary : " + emp.collectPay());
+		emp.terminate("2025-12-31");
+		emp.retire();
+	}
+}
---
+++ rachitsingh/practice/OOP_Module/WorkerEmployeeChallenge/SalariedEmployee.java
@@ -0,0 +1,22 @@
+package WorkerEmployeeChallenge;
+
+public class SalariedEmployee extends Employee{
+	private double annualSalary;
+	private boolean isRetired = false;
+	
+	public SalariedEmployee(String name, String birthDate, long employeeId, String hireDate, double annualSalary)
+	{
+		super(name, birthDate, employeeId, hireDate);
+		this.annualSalary = annualSalary;
+	}
+	@Override
+	public double collectPay()
+	{
+		return annualSalary/12; // monthly pay
+	}
+	public void retire()
+	{
+		isRetired = true;
+		System.out.println(getName() + " has reached the retirement stage.");
+	}
+}
