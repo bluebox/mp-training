44aaf36992a9d948790ca7f448b6070007dccdf8
k-Rakeshram <124486831+k-Rakeshram@users.noreply.github.com>
2025-06-09 20:25:35 +0530

    Add files via upload

---
+++ tasks/CaseStudy2/Employees.java
@@ -0,0 +1,39 @@
+package CaseStudy2;
+
+import java.time.LocalDate;
+
+public class Employees {
+    private String employeeId;
+    private String name;
+    private String department;
+    private String projectId;
+    private LocalDate date;
+    private String taskCategory;
+    private double hoursWorked;
+    private String remarks;
+
+    public Employees(String employeeId, String name, String department, String projectId,
+                           LocalDate date, String taskCategory, double hoursWorked, String remarks) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.projectId = projectId;
+        this.date = date;
+        this.taskCategory = taskCategory;
+        this.hoursWorked = hoursWorked;
+        this.remarks = remarks;
+    }
+
+    public String getEmployeeId() { return employeeId; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public String getProjectId() { return projectId; }
+    public LocalDate getDate() { return date; }
+    public String getTaskCategory() { return taskCategory; }
+    public double getHoursWorked() { return hoursWorked; }
+    public String getRemarks() { return remarks; }
+    @Override
+    public String toString() {
+    	return employeeId+"  "+name+"  "+department+" "+projectId+" "+date+" "+taskCategory+" "+hoursWorked+" "+remarks;
+    }
+}
