11964d3c33fc6d0f44f69e2d37b34e8d5c186d92
Surya Chandra Teja <suryachandrateja444@gmail.com>
2025-06-10 20:24:10 +0530

    implemented the challenge Minimum finder

---
+++ training/java/core/Challenges/ImplementMinFinder/ImplementMinFinder.java
@@ -0,0 +1,39 @@
+package training.java.core.Challenges.ImplementMinFinder;
+
+import java.util.Scanner;
+
+public class ImplementMinFinder {
+
+    private static int readInteger(Scanner sc) {
+        System.out.println("Enter the number of elements:");
+        return sc.nextInt();
+    }
+
+    private static int[] readElements(int x, Scanner sc) {
+        System.out.println("Enter the elements:");
+        int[] a = new int[x];
+        for (int i = 0; i < x; i++) {
+            a[i] = sc.nextInt();
+        }
+        return a;
+    }
+
+    private static int findMin(int[] a) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
+        return min;
+    }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int z = readInteger(sc);
+    //     int[] array = readElements(z, sc);
+    //     int minValue = findMin(array);
+    //     System.out.println("The minimum value is: " + minValue);
+    //     sc.close();
+    // }
+}
