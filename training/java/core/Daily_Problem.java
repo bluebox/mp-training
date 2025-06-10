814447a9b2f6388c13217c56241c3e7e03d18c05
123kanishka <112252443+123kanishka@users.noreply.github.com>
2025-06-10 20:25:23 +0530

    Add files via upload

---
+++ training/java/core/Daily_Problem.java
@@ -0,0 +1,23 @@
+package package_1;
+
+public class Daily_Problem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tree treeObj=new Tree();
+		treeObj.addNode(100);
+		treeObj.addNode(20);
+		treeObj.addNode(200);
+		treeObj.addNode(10);
+		treeObj.addNode(30);
+		treeObj.addNode(150);
+		treeObj.addNode(300);
+
+		treeObj.Printinorder();
+		treeObj.Printpostorder();
+		treeObj.Printpreorder();
+		treeObj.level_order();
+
+	}
+
+}
