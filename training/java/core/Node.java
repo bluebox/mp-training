17e0f3c3264bcf058aede4b0e335de5c1c9b6ce9
123kanishka <112252443+123kanishka@users.noreply.github.com>
2025-06-10 17:54:17 +0530

    Add files via upload

---
+++ training/java/core/Node.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Node {
+	Node left;
+	Node right;
+	int value;
+	Node()
+	{
+		
+	}
+	Node(int value) {
+		this.left=null;
+		this.right=null;
+		this.value=value;
+	}
+
+}
