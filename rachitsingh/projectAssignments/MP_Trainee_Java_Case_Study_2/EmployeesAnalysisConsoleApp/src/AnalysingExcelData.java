cf95ad382f38684af9da282946ccb8eb76826a93
Medplus Developer <dev@medplus.com>
2025-06-10 20:50:27 +0530

    (Extra) average weekly hours grouped by employee and month - data saved

--- rachitsingh/projectAssignments/MP_Trainee_Java_Case_Study_2/EmployeesAnalysisConsoleApp/src/AnalysingExcelData.java
+++ rachitsingh/projectAssignments/MP_Trainee_Java_Case_Study_2/EmployeesAnalysisConsoleApp/src/AnalysingExcelData.java
@@ -2,7 +2,7 @@ package EmployeesAnalysisConsoleApp.src;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.time.YearMonth;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -12,84 +12,123 @@ import java.util.stream.Collectors;
 
 public class AnalysingExcelData {
 
-    // Query-1.  Sort by task category and hours descending, top 3 time-consuming tasks per department.
-    public static Map<String, List<EmployeesWorkLogPOJO>> getTopTasksPerDepartment(List<EmployeesWorkLogPOJO> employeeLogs)
-    {
-        Map<String, List<EmployeesWorkLogPOJO>> departmentToLogsMap = employeeLogs.stream()
-                .collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getDepartment));
+	// Query-1. Sort by task category and hours descending, top 3 time-consuming
+	// tasks per department.
+	public static Map<String, List<EmployeesWorkLogPOJO>> getTopTasksPerDepartment(
+			List<EmployeesWorkLogPOJO> employeeLogs) {
+		Map<String, List<EmployeesWorkLogPOJO>> departmentToLogsMap = employeeLogs.stream()
+				.collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getDepartment));
 
-        Map<String, List<EmployeesWorkLogPOJO>> queryResult = new HashMap<>();
+		Map<String, List<EmployeesWorkLogPOJO>> queryResult = new HashMap<>();
 
-        for (Map.Entry<String, List<EmployeesWorkLogPOJO>> mapEntry : departmentToLogsMap.entrySet()) {
-            String department = mapEntry.getKey();
-            List<EmployeesWorkLogPOJO> departmentLogs = mapEntry.getValue();
+		for (Map.Entry<String, List<EmployeesWorkLogPOJO>> mapEntry : departmentToLogsMap.entrySet()) {
+			String department = mapEntry.getKey();
+			List<EmployeesWorkLogPOJO> departmentLogs = mapEntry.getValue();
 
-            List<EmployeesWorkLogPOJO> topThreeTasks = departmentLogs.stream()
-                    .sorted(Comparator.comparing(EmployeesWorkLogPOJO::getTaskCategory)
-                            .thenComparing(Comparator.comparing(EmployeesWorkLogPOJO::getHoursWorked).reversed()))
-                    .limit(3)
-                    .collect(Collectors.toList());
+			List<EmployeesWorkLogPOJO> topThreeTasks = departmentLogs.stream()
+					.sorted(Comparator.comparing(EmployeesWorkLogPOJO::getTaskCategory)
+							.thenComparing(Comparator.comparing(EmployeesWorkLogPOJO::getHoursWorked).reversed()))
+					.limit(3).collect(Collectors.toList());
 
-            queryResult.put(department, topThreeTasks);
-        }
-        return queryResult;
-    }
-    
-    // Query-2. Weekend logs, summarize weekend hours.
-    public static double computeWeekendWorkHours(List<EmployeesWorkLogPOJO> employeeLogs)
-    {
-        return employeeLogs.stream().filter(
-                employeeLog -> {
-                    DayOfWeek day = employeeLog.getDate().getDayOfWeek();
-                    return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
-                })
-                .map(EmployeesWorkLogPOJO::getHoursWorked)
-                .reduce(0.0, Double::sum);
-    }
-    
-    // Query-3. Daily average hours trend in last 30 working days per employee.
-    public static Map<String, Double> getDailyAverageWorkingHours30DaysPerEmployee(List<EmployeesWorkLogPOJO> employeeLogs)
-    {
-        LocalDate thirtyDaysBefore = LocalDate.now().minusDays(30);
+			queryResult.put(department, topThreeTasks);
+		}
+		return queryResult;
+	}
 
-        return employeeLogs.stream()
-                .filter(employeeLog -> !employeeLog.getDate().isBefore(thirtyDaysBefore))
-                .collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getEmployeeID,
-                        Collectors.averagingDouble(EmployeesWorkLogPOJO::getHoursWorked)));
+	// Query-2. Weekend logs, summarize weekend hours.
+	public static double computeWeekendWorkHours(List<EmployeesWorkLogPOJO> employeeLogs) {
+		double[] totalWeekendHours = { 0.0 };
+		employeeLogs.stream().forEach(employeeLog -> {
+			if (employeeLog.getDate().getDayOfWeek() == DayOfWeek.SATURDAY
+					|| employeeLog.getDate().getDayOfWeek() == DayOfWeek.SUNDAY) {
+				totalWeekendHours[0] += employeeLog.getHoursWorked();
+			}
+		});
+		return totalWeekendHours[0];
 
-    }
-    //Query-4. Days with <2 hours, group by employee and date.
-    public static Map<String, List<LocalDate>> getLessthanTwoHourWorkDaysData(List<EmployeesWorkLogPOJO> employeeLogs)
-    {
-        Map<String, List<LocalDate>> queryResult = new HashMap<>();
-        employeeLogs.stream()
-                .filter(employeeLog -> employeeLog.getHoursWorked() < 2.0)
-                .forEach(employeeLog -> {
-                    queryResult.computeIfAbsent(employeeLog.getEmployeeID(), k -> new ArrayList<>())
-                            .add(employeeLog.getDate());
-                });
-        return queryResult;
-    }
-    // Query-5. Time % spent in each task category per employee
-    public static Map<String, Map<String, Double>> getPercentTimeSpentTaskCategoryWisePerEmployee(List<EmployeesWorkLogPOJO> employeeLogs)
-    {
-    	Map<String, Map<String, Double>> groupedByEIDandTaskMap = new HashMap<>();
-  
-    	Map<String, List<EmployeesWorkLogPOJO>> logsGroupedByEmployee = employeeLogs.stream()
-    			.collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getEmployeeID));
-    	
-    	for (Map.Entry<String, List<EmployeesWorkLogPOJO>> entry : logsGroupedByEmployee.entrySet())
-    	{
-    	    groupedByEIDandTaskMap.put(entry.getKey(), getTimeSpentByEmployee(entry.getValue()));
-    	}
-    	return groupedByEIDandTaskMap;
-    }
-    
-    private static Map<String, Double> getTimeSpentByEmployee(List<EmployeesWorkLogPOJO> workLogs) {
-    	Double totalTimeSpent = workLogs.stream().mapToDouble(EmployeesWorkLogPOJO::getHoursWorked).sum();
-    	Map<String, Double> timeSpentPerCategory = workLogs.stream().collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getTaskCategory, Collectors.summingDouble(EmployeesWorkLogPOJO::getHoursWorked)));
-    	return timeSpentPerCategory.entrySet().stream().collect(Collectors.toMap(Entry::getKey, entry -> {
-    		return entry.getValue() / totalTimeSpent * 100;
-    	}));
-    }
+//		return employeeLogs.stream().filter(employeeLog -> {
+//			DayOfWeek day = employeeLog.getDate().getDayOfWeek();
+//			return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+//		}).map(EmployeesWorkLogPOJO::getHoursWorked).reduce(0.0, Double::sum);
+	}
+
+	// Query-3. Daily average hours trend in last 30 working days per employee.
+	public static Map<String, Double> getDailyAverageWorkingHours30DaysPerEmployee(
+			List<EmployeesWorkLogPOJO> employeeLogs) {
+		LocalDate thirtyDaysBefore = LocalDate.now().minusDays(30);
+
+		return employeeLogs.stream().filter(employeeLog -> !employeeLog.getDate().isBefore(thirtyDaysBefore))
+				.collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getEmployeeID,
+						Collectors.averagingDouble(EmployeesWorkLogPOJO::getHoursWorked)));
+
+	}
+
+	// Query-4. Days with <2 hours, group by employee and date.
+	public static Map<String, List<LocalDate>> getLessthanTwoHourWorkDaysData(List<EmployeesWorkLogPOJO> employeeLogs) {
+		Map<String, List<LocalDate>> queryResult = new HashMap<>();
+//		employeeLogs.stream().filter(employeeLog -> employeeLog.getHoursWorked() < 2.0).forEach(employeeLog -> {
+//			queryResult.computeIfAbsent(employeeLog.getEmployeeID(), k -> new ArrayList<>()).add(employeeLog.getDate());
+//		});
+
+		queryResult = employeeLogs.stream().filter(employeeLog -> employeeLog.getHoursWorked() < 2.0)
+				.collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getEmployeeID,
+						Collectors.mapping(EmployeesWorkLogPOJO::getDate, Collectors.toList())));
+		return queryResult;
+	}
+
+	// Query-5. Time % spent in each task category per employee
+	public static Map<String, Map<String, Double>> getPercentTimeSpentTaskCategoryWisePerEmployee(
+			List<EmployeesWorkLogPOJO> employeeLogs) {
+		Map<String, Map<String, Double>> groupedByEIDandTaskMap = new HashMap<>();
+
+		Map<String, List<EmployeesWorkLogPOJO>> logsGroupedByEmployee = employeeLogs.stream()
+				.collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getEmployeeID));
+
+		for (Map.Entry<String, List<EmployeesWorkLogPOJO>> entry : logsGroupedByEmployee.entrySet()) {
+			groupedByEIDandTaskMap.put(entry.getKey(), getTimeSpentByEmployee(entry.getValue()));
+		}
+		return groupedByEIDandTaskMap;
+	}
+
+	private static Map<String, Double> getTimeSpentByEmployee(List<EmployeesWorkLogPOJO> workLogs) {
+		Double totalTimeSpent = workLogs.stream().mapToDouble(EmployeesWorkLogPOJO::getHoursWorked).sum();
+		Map<String, Double> timeSpentPerCategory = workLogs.stream().collect(Collectors.groupingBy(
+				EmployeesWorkLogPOJO::getTaskCategory, Collectors.summingDouble(EmployeesWorkLogPOJO::getHoursWorked)));
+		return timeSpentPerCategory.entrySet().stream().collect(Collectors.toMap(Entry::getKey, entry -> {
+			return entry.getValue() / totalTimeSpent * 100;
+		}));
+	}
+
+	// Query-6: Group by Employee and month; compute average weekly hours;
+	public static Map<String, Map<YearMonth, Double>> getAverageWeeklyHoursGroupByEmployeeAndMonth(
+			List<EmployeesWorkLogPOJO> workLogs) {
+		
+		//Grouping done by employeeID
+		Map<String, List<EmployeesWorkLogPOJO>> workLogsGroupedByEmployeeID = workLogs.stream()
+				.collect(Collectors.groupingBy(EmployeesWorkLogPOJO::getEmployeeID));
+		
+		Map<String, Map<YearMonth, List<EmployeesWorkLogPOJO>>> employeeIDGroupedWorkLogsGroupedByMonth = new HashMap<>();
+		
+		//Further grouping of employeeID grouped data by Month
+		workLogsGroupedByEmployeeID.forEach((employeeID, employeeWorkLogs) -> {
+			Map<YearMonth, List<EmployeesWorkLogPOJO>> employeeWorkLogsGroupedByMonth = employeeWorkLogs.stream()
+					.collect(Collectors.groupingBy(employeeWorkLog -> YearMonth.from(employeeWorkLog.getDate())));
+			
+			employeeIDGroupedWorkLogsGroupedByMonth.put(employeeID, employeeWorkLogsGroupedByMonth);
+		});
+		
+		Map<String,Map<YearMonth, Double>> queryResult = new HashMap<>();
+		for(Map.Entry<String,Map<YearMonth, List<EmployeesWorkLogPOJO>>> entry: employeeIDGroupedWorkLogsGroupedByMonth.entrySet())
+		{
+			Map<YearMonth, Double> monthlyHoursAverage = entry.getValue().entrySet().stream().collect(Collectors.toMap(Entry::getKey, currentEntry -> {
+				return (currentEntry.getValue().stream().map(EmployeesWorkLogPOJO::getHoursWorked).mapToDouble(Double::doubleValue).sum()/noOfWeeksInMonth(currentEntry.getKey()));
+			}));
+			queryResult.put(entry.getKey(), monthlyHoursAverage);
+		};
+		return queryResult;
+	}
+	
+	private static int noOfWeeksInMonth(YearMonth month) {
+		return month.getMonth().minLength() / 7;
+	}
 }
--- rachitsingh/projectAssignments/MP_Trainee_Java_Case_Study_2/EmployeesAnalysisConsoleApp/src/ExcelFileWriter.java
+++ rachitsingh/projectAssignments/MP_Trainee_Java_Case_Study_2/EmployeesAnalysisConsoleApp/src/ExcelFileWriter.java
@@ -2,103 +2,101 @@ package EmployeesAnalysisConsoleApp.src;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class ExcelFileWriter 
-{
-    private static DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+public class ExcelFileWriter {
+	private static DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    public static void writeTopThreeTasks(Map<String, List<EmployeesWorkLogPOJO>> data, String filePathName)
-    {
-        try (FileWriter fileWriter = new FileWriter(filePathName)) {
-            fileWriter.write("Department,Employee ID,Name,Task Category,Hours Worked,Date\n");
-            for (Map.Entry<String, List<EmployeesWorkLogPOJO>> mapEntry : data.entrySet()) {
-                for (EmployeesWorkLogPOJO employeeLog : mapEntry.getValue()) {
-                    StringBuilder record = new StringBuilder();
-                    record.append(mapEntry.getKey()).append(",")
-                            .append(employeeLog.getEmployeeID()).append(",")
-                            .append(employeeLog.getemployeeName()).append(",")
-                            .append(employeeLog.getTaskCategory()).append(",")
-                            .append(employeeLog.getHoursWorked()).append(",")
-                            .append(employeeLog.getDate().format(DATE_FORMAT)).append("\n");
-                    fileWriter.write(record.toString());
-                }
-            }
-        } catch (IOException IOE) {
-            System.err.println("Error occured while writing top 3 tasks: " + IOE.getMessage());
-        }
-    }
-    
-    public static void writeWeekendSummary(double totalHours, String filePathName)
-    {
-        try (FileWriter fileWriter = new FileWriter(filePathName)) 
-        {
-            fileWriter.write("Total hours worked in the weeked:\n");
-            fileWriter.write(String.valueOf(totalHours) + "\n");
-        } 
-        catch (IOException IOE) 
-        {
-            System.err.println("Error occured while writing the summary of weekend: " + IOE.getMessage());
-        }
-    }
-    
-    public static void writeDailyAverage(Map<String, Double> data, String filePathName)
-    {
-        try (FileWriter fileWriter = new FileWriter(filePathName)) 
-        {
-            fileWriter.write("Employee ID, Average Hours Worked (Last 30 days)\n");
-            for (Map.Entry<String, Double> mapEntry : data.entrySet()) {
-                fileWriter.write(mapEntry.getKey() + "," + mapEntry.getValue() + "\n");
-            }
-        } 
-        catch (IOException IOE) 
-        {
-            System.err.println("Error occured while writing daily average working hours: " + IOE.getMessage());
-        }
-    }
-    
-    public static void writeLessthanTwoHourWorkDaysData(Map<String, List<java.time.LocalDate>> data, String filePathName)
-    {
-        try(FileWriter fileWriter = new FileWriter(filePathName))
-        {
-            fileWriter.write("Employee ID, Dates with <2 working hours\n");
+	public static void writeTopThreeTasks(Map<String, List<EmployeesWorkLogPOJO>> data, String filePathName) {
+		try (FileWriter fileWriter = new FileWriter(filePathName)) {
+			fileWriter.write("Department,Employee ID,Name,Task Category,Hours Worked,Date\n");
+			for (Map.Entry<String, List<EmployeesWorkLogPOJO>> mapEntry : data.entrySet()) {
+				for (EmployeesWorkLogPOJO employeeLog : mapEntry.getValue()) {
+					StringBuilder record = new StringBuilder();
+					record.append(mapEntry.getKey()).append(",").append(employeeLog.getEmployeeID()).append(",")
+							.append(employeeLog.getemployeeName()).append(",").append(employeeLog.getTaskCategory())
+							.append(",").append(employeeLog.getHoursWorked()).append(",")
+							.append(employeeLog.getDate().format(DATE_FORMAT)).append("\n");
+					fileWriter.write(record.toString());
+				}
+			}
+		} catch (IOException IOE) {
+			System.err.println("Error occured while writing top 3 tasks: " + IOE.getMessage());
+		}
+	}
 
-            for (Map.Entry<String, List<java.time.LocalDate>> mapEntry : data.entrySet()) {
-                String allCombinedDates = mapEntry.getValue().stream()
-                        .map(date -> date.format(DATE_FORMAT))
-                        .collect(Collectors.joining("; "));
+	public static void writeWeekendSummary(double totalHours, String filePathName) {
+		try (FileWriter fileWriter = new FileWriter(filePathName)) {
+			fileWriter.write("Total hours worked in the weeked:\n");
+			fileWriter.write(String.valueOf(totalHours) + "\n");
+		} catch (IOException IOE) {
+			System.err.println("Error occured while writing the summary of weekend: " + IOE.getMessage());
+		}
+	}
 
-                fileWriter.write(mapEntry.getKey() + "," + allCombinedDates + "\n");
-            }
-        }
-        catch (IOException IOE) 
-        {
-            System.err.println("Error occured while writing <2 hour days: " + IOE.getMessage());
-        }
-    }
-    
-    public static void writePercentTimeSpentTaskCategoryWisePerEmployee(Map<String, Map<String, Double>> data, String filePathName)
-    {
-    	try(FileWriter fileWriter = new FileWriter(filePathName))
-    	{
-    		fileWriter.write("Employee ID, Task Category, % Time Spent\n");
-    		 for (Map.Entry<String, Map<String, Double>> employeeEntry : data.entrySet())
-    		 {
-    	            String employeeID = employeeEntry.getKey();
-    	            Map<String, Double> taskToPercentMap = employeeEntry.getValue();
+	public static void writeDailyAverage(Map<String, Double> data, String filePathName) {
+		try (FileWriter fileWriter = new FileWriter(filePathName)) {
+			fileWriter.write("Employee ID, Average Hours Worked (Last 30 days)\n");
+			for (Map.Entry<String, Double> mapEntry : data.entrySet()) {
+				fileWriter.write(mapEntry.getKey() + "," + mapEntry.getValue() + "\n");
+			}
+		} catch (IOException IOE) {
+			System.err.println("Error occured while writing daily average working hours: " + IOE.getMessage());
+		}
+	}
 
-    	            for (Map.Entry<String, Double> taskEntry : taskToPercentMap.entrySet())
-    	            {
-    	                fileWriter.write(employeeID + "," + taskEntry.getKey() + "," + taskEntry.getValue() + "\n");
-    	            }
-    	        } 
-    	}
-    	catch (IOException IOE)
-    	{
-	        System.err.println("Error occurred while writing Task Category wise Percentage data: " + IOE.getMessage());
-	    }
-    }
+	public static void writeLessthanTwoHourWorkDaysData(Map<String, List<java.time.LocalDate>> data,
+			String filePathName) {
+		try (FileWriter fileWriter = new FileWriter(filePathName)) {
+			fileWriter.write("Employee ID, Dates with <2 working hours\n");
+
+			for (Map.Entry<String, List<java.time.LocalDate>> mapEntry : data.entrySet()) {
+				String allCombinedDates = mapEntry.getValue().stream().map(date -> date.format(DATE_FORMAT))
+						.collect(Collectors.joining("; "));
+
+				fileWriter.write(mapEntry.getKey() + "," + allCombinedDates + "\n");
+			}
+		} catch (IOException IOE) {
+			System.err.println("Error occured while writing <2 hour days: " + IOE.getMessage());
+		}
+	}
+
+	public static void writePercentTimeSpentTaskCategoryWisePerEmployee(Map<String, Map<String, Double>> data,
+			String filePathName) {
+		try (FileWriter fileWriter = new FileWriter(filePathName)) {
+			fileWriter.write("Employee ID, Task Category, % Time Spent\n");
+			for (Map.Entry<String, Map<String, Double>> employeeEntry : data.entrySet()) {
+				String employeeID = employeeEntry.getKey();
+				Map<String, Double> taskToPercentMap = employeeEntry.getValue();
+				for (Map.Entry<String, Double> taskEntry : taskToPercentMap.entrySet()) {
+					fileWriter.write(employeeID + "," + taskEntry.getKey() + "," + taskEntry.getValue() + "\n");
+				}
+			}
+		} catch (IOException IOE) {
+			System.err.println("Error occurred while writing Task Category wise Percentage data: " + IOE.getMessage());
+		}
+	}
+	
+	public static void writeAverageWeeklyHoursGroupByEmployeeAndMonth(Map<String, Map<YearMonth, Double>> data, String filePathName) {
+		try(FileWriter fileWriter = new FileWriter(filePathName)){
+			fileWriter.write("Employee ID, Month, Weekly working hours average\n");
+			for(Map.Entry<String, Map<YearMonth, Double>> employeeEntry : data.entrySet()) {
+				String employeeID = employeeEntry.getKey();
+				Map<YearMonth, Double> averageWeeklyHoursMap = employeeEntry.getValue();
+				
+				for(Map.Entry<YearMonth, Double> monthlyHoursEntry: averageWeeklyHoursMap.entrySet()) {
+					fileWriter.write(employeeID + "," + monthlyHoursEntry.getKey() + "," + monthlyHoursEntry.getValue() + "\n");
+				}
+			}
+		}
+		catch (IOException IOE) {
+			System.err.println("Error occurred while writing Monthly weekly hours average per employee data: " + IOE.getMessage());
+		}
+		
+	}
+
 }
