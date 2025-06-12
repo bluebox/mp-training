1300b9d7c92a1e5a52d9f4d124c43a59d08908fa
Manojreddy19 <manojreddykadipikonda2002@gmail.com>
2025-06-12 19:58:53 +0530

    Added Batch Processing

--- training/java/core/DateAndTime/src/LocalTimeUsage.java
+++ training/java/core/DateAndTime/src/LocalTimeUsage.java
@@ -1,14 +1,18 @@
 import java.time.Duration;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 import java.util.concurrent.TimeUnit;
 
 public class LocalTimeUsage {
 	public static void main(String[] args) throws InterruptedException {
-		LocalTime current=LocalTime.now();
+		LocalTime current = LocalTime.now();
 		System.out.println(current);
-			//TimeUnit.SECONDS.sleep(5);
-		int hours=(int) TimeUnit.SECONDS.toHours(3670);
-			
+		// TimeUnit.SECONDS.sleep(5);
+		int hours = (int) TimeUnit.SECONDS.toHours(3670);
+		System.out.println(current.get(ChronoField.CLOCK_HOUR_OF_DAY));
+		System.out.println("Minutes " + current.getMinute());
+
 		System.out.println(hours);
 	}
 
