25bac53e8bb18e365c789322fc313cd48302f833
Manoj Reddy <manojreddykadipikonda2002@gmail.com>
2025-06-14 14:52:08 +0530

    UI is completed for library management case study

---
+++ tasks/LibraryManagemet/src/com/library/controller/HomeController.java
@@ -0,0 +1,50 @@
+package com.library.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class HomeController {
+
+	private void loadScene(String fxmlFile, ActionEvent event) throws IOException {
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/library/UI/" + fxmlFile));
+	    Scene scene = new Scene(loader.load());
+	    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	    stage.setScene(scene);
+	    stage.setTitle("Library System");
+	    stage.show();
+	}
+
+
+    @FXML private void goToAddBook(ActionEvent event) throws IOException {
+        loadScene("AddBook.fxml", event);
+    }
+
+    @FXML private void goToAddMember(ActionEvent event) throws IOException {
+        loadScene("AddMember.fxml", event);
+    }
+
+    @FXML private void goToViewBooks(ActionEvent event) throws IOException {
+        loadScene("ViewBooks.fxml", event);
+    }
+
+    @FXML private void goToViewMembers(ActionEvent event) throws IOException {
+        loadScene("ViewMembers.fxml", event);
+    }
+
+    @FXML private void goToIssueBook(ActionEvent event) throws IOException {
+        loadScene("IssueBook.fxml", event);
+    }
+
+    @FXML private void goToReturnBook(ActionEvent event) throws IOException {
+        loadScene("ReturnBook.fxml", event);
+    }
+
+    @FXML private void goToIssuedRecords(ActionEvent event) throws IOException {
+        loadScene("IssuedRecords.fxml", event);
+    }
+}
