7fa14e37d18482217129c898f003f3771c2a189a
Manoj Reddy <manojreddykadipikonda2002@gmail.com>
2025-06-14 16:15:36 +0530

    Added Member POJO and MYSQL Connection

--- tasks/LibraryManagemet/src/com/library/controller/IssueBookController.java
+++ tasks/LibraryManagemet/src/com/library/controller/IssueBookController.java
@@ -1,7 +1,9 @@
 package com.library.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class IssueBookController {
 
---
+++ tasks/LibraryManagemet/src/com/library/dao/AddMember.java
@@ -0,0 +1,6 @@
+package com.library.dao;
+
+public class AddMember {
+	
+
+}
