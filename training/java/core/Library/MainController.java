6a43ac8d228d7a77be7fdc5869de586efdb04eb2
Chaitanya-1104 <93830104+Chaitanya-1104@users.noreply.github.com>
2025-06-15 17:28:13 +0530

    Update MainController.java

--- training/java/core/Library/MainController.java
+++ training/java/core/Library/MainController.java
@@ -41,23 +41,15 @@ public class MainController {
     @FXML
     private void handleAddMember(ActionEvent event) {
         System.out.println("Add Member button clicked");
-        // Add your code here to open Add Member UI
     }
 
     @FXML
     private void handleIssueReturn(ActionEvent event) {
         System.out.println("Issue/Return button clicked");
-        // Add your code here to open Issue/Return UI
     }
 
     @FXML
     private void handleReports(ActionEvent event) {
         System.out.println("Reports button clicked");
-        // Add your code here to open Reports UI
-        // use enums for active and inacctive ,available not availabale and gender
-        // feiilds
-        // try to use concepts of core java like inner classess,generics,interfaces and
-        // abstract classess
-
     }
 }
