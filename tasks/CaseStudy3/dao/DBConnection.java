fa30b96cd140dba80e5c8ba79706a90de86b282c
nikasbandaru <prasad.ece3.083@gmail.com>
2025-06-16 19:41:13 +0530

    html

--- tasks/CaseStudy3/dao/DBConnection.java
+++ tasks/CaseStudy3/dao/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/library_db";
-    private static final String USER = "root";  // Change this if your MySQL user is different
-    private static final String PASS = "your_password"; // Replace with your MySQL password
+    private static final String USER = "Prasad";  // Change this if your MySQL user is different
+    private static final String PASS = "Welcome123"; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
