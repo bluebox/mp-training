23f7dd8995f8d9b875e4b41859d933e657b8daf6
revanth-g <82812901+revanth-g@users.noreply.github.com>
2025-06-16 10:08:13 +0530

    updated

---
+++ training/java/core/MyPractice/src/june5_regularexpressions/EmailValidatorChallengeTest.java
@@ -0,0 +1,16 @@
+package june5_regularexpressions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmailValidatorChallengeTest {
+	@Test
+	void testMain() {
+		assertNotNull("Not yet implemented");
+	}
+	@Test
+	void check() {
+		assertEquals(12,12);
+	}
+}
--- training/java/core/MyPractice/src/module-info.java
+++ training/java/core/MyPractice/src/module-info.java
@@ -6,4 +6,5 @@
  */
 module MyPractice {
 	requires java.sql;
+	requires org.junit.jupiter.api;
 }
