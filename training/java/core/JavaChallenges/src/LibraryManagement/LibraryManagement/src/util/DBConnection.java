c2ec2becd49f1276b023fb754f6acbcc19a20b83
PULI UDAY KIRAN <119495444+uday-9949@users.noreply.github.com>
2025-06-16 12:16:36 +0530

    Updated

--- training/java/core/JavaChallenges/src/LibraryManagement/LibraryManagement/src/util/DBConnection.java
+++ training/java/core/JavaChallenges/src/LibraryManagement/LibraryManagement/src/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/bhanu";
+    private static final String URL = "jdbc:mysql://localhost:3306/uday";
     private static final String USER = "practice";
-    private static final String PASSWORD = "Vbhanu@123"; 
+    private static final String PASSWORD = "uday@123"; 
 
     static {
         try {
