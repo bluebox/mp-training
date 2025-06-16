9befdbe50e2821200178e102f9fb038fe06e185c
Niranjan2114 <144309125+Niranjan2114@users.noreply.github.com>
2025-06-16 17:23:44 +0530

    Update IssueService.java

--- tasks/LibraryManagementSystem/src/service/IssueService.java
+++ tasks/LibraryManagementSystem/src/service/IssueService.java
@@ -8,14 +8,14 @@ import model.Book;
 import model.IssueRecord;
 public class IssueService {
     private final IssueDAO issueDAO = new IssueDAO();
-    private final BookDAO bookDAO = new BookDAO(); // to fetch book status
+    private final BookDAO bookDAO = new BookDAO(); 
 
     public void issueBook(IssueRecord issue) throws Exception {
         if (issue.getBookId() <= 0) throw new IllegalArgumentException("Invalid Book ID");
         if (issue.getMemberId() <= 0) throw new IllegalArgumentException("Invalid Member ID");
         if (issue.getStatus() != 'I') throw new IllegalArgumentException("Issue status must be 'A'");
 
-        // Step 1: Check book availability
+      
         Book book = bookDAO.getBookById(issue.getBookId());
         if (book == null) {
             throw new IllegalArgumentException("Book not found.");
@@ -24,7 +24,6 @@ public class IssueService {
             throw new IllegalStateException("Book is already issued.");
         }
 
-        // Step 2: Proceed with issuing the book (inserts + updates inside DAO)
         issueDAO.issueBook(issue);
     }
 }
