834a24b44162b3c9dbcc2084daa6b597423dcbaa
dharani17chintha <160999182+dharani17chintha@users.noreply.github.com>
2025-06-16 08:28:38 +0530

    Update MainTest.java

--- training/java/core/JUnitTesting/test/MainTest.java
+++ training/java/core/JUnitTesting/test/MainTest.java
@@ -1,1 +1,17 @@
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class MainTest {
+    Main main=new Main();
+    @Test
+    public void testAdd(){
+        assertEquals(5,main.add(2,3));
+    }
+    @Test
+    public void testSub(){
+        assertEquals(4,main.sub(10,6));
+    }
+
+}
