91045ff95a3ad7990bab54017faf2c2bf6f57a2e
PULI UDAY KIRAN <119495444+uday-9949@users.noreply.github.com>
2025-06-16 12:14:02 +0530

    Updated

--- training/java/core/JavaChallenges/src/LibraryManagement/LibraryManagement/src/dao/BookDAO.java
+++ training/java/core/JavaChallenges/src/LibraryManagement/LibraryManagement/src/dao/BookDAO.java
@@ -13,7 +13,6 @@ import enums.Status;
 
 public class BookDAO {
 
-    // Create (Add Book)
     public void addBook(Book book) throws DatabaseException {
         String sql = "INSERT INTO books (title, author, category, status, availability) VALUES (?, ?, ?, ?, ?)";
         try (Connection conn = DBConnection.getConnection();
@@ -31,7 +30,6 @@ public class BookDAO {
         }
     }
 
-    // Read (Get All Books)
     public List<Book> getAllBooks() throws DatabaseException {
         List<Book> books = new ArrayList<>();
         String sql = "SELECT * FROM books";
@@ -59,7 +57,6 @@ public class BookDAO {
         return books;
     }
 
-    // Update Book Details (except availability)
     public void updateBookDetails(Book book) throws DatabaseException {
         String sql = "UPDATE books SET title=?, author=?, category=?, status=? WHERE bookId=?";
         try (Connection conn = DBConnection.getConnection();
@@ -77,7 +74,6 @@ public class BookDAO {
         }
     }
 
-    // Update Availability Only
     public void updateAvailability(int bookId, char availability) throws DatabaseException {
         String sql = "UPDATE books SET availability=? WHERE bookId=?";
         try (Connection conn = DBConnection.getConnection();
@@ -91,8 +87,6 @@ public class BookDAO {
             throw new DatabaseException("Error updating book availability: " + e.getMessage(), e);
         }
     }
-
-    // Check if Book is Available
     public boolean isBookAvailable(int bookId) throws DatabaseException {
         String sql = "SELECT availability FROM books WHERE bookId=?";
         try (Connection conn = DBConnection.getConnection();
@@ -111,7 +105,6 @@ public class BookDAO {
             throw new DatabaseException("Error checking book availability: " + e.getMessage(), e);
         }
     }
- // Get Book by ID
     public Book getBookById(int bookId) throws DatabaseException {
         String sql = "SELECT * FROM books WHERE bookId = ?";
         try (Connection conn = DBConnection.getConnection();
