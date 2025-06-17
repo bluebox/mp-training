9629c0e5e5c0f42abc49564b06425b3494e32cbd
Sri Sathvika <srisathvika02@gmail.com>
2025-06-17 19:49:42 +0530

    js

---
+++ training/javascript/JS/script.js
@@ -0,0 +1,72 @@
+// function wordBlanks(noun,adj,verb,adverb){
+//     var ans="";
+//     ans+="The "+adj+" "+noun+" "+verb+" to the store "+adverb;
+//     console.log(ans);
+// }
+
+// wordBlanks("clever","dog","ran","fast");
+
+// console.log("Hello World!!")
+
+// var myName="name"
+// console.log(myName)
+// myName=8
+// console.log(myName)
+
+// let title="this is title"
+// console.log(title)
+// const pi=3.14
+// console.log(pi)
+// // pi=9
+// // console.log(pi)
+
+// var myStr="Iam a \"double qouted\" string in \"double quotes\""
+// console.log(myStr)
+
+// myStr="FirstLIne\n\t\\second line\n third line"
+// console.log(myStr)
+
+//pop- remove last ele
+//shift- remove first ele
+// var array=[1,2,3,4,5]
+// console.log(array.pop())
+// console.log(array)
+// console.log(array.shift())
+// console.log(array)
+// array.unshift(1);
+// console.log(array)
+
+
+// array=[["pandu",45],["nik",90]]
+// console.log(array.pop());
+// console.log(array)
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(3,4));
+
+// function nextInLIne(arr,item){
+//     //QUEUE BEHAVIOUR
+//     arr.push(item);
+//     return arr.shift();
+// }
+
+// var testArr=[1,2,3,4,5,6];
+// console.log("Before: "+JSON.stringify(testArr));
+// console.log(nextInLIne(testArr,7));
+// console.log("After: "+JSON.stringify(testArr));
+
+
+// var testObj={
+//     "name":"pandu",
+//     "age":15,
+//     "dob":"may 3"
+// };
+
+// console.log(testObj.age);
+// console.log(testObj.name);
+// console.log(testObj.dob);
+
+var add=(a,b)=>(a+b);
+console.log(add(2,3))
