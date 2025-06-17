e684a189ae0c682c7958d016c80ba0fc8355ce61
nikasbandaru <prasad.ece3.083@gmail.com>
2025-06-17 15:57:17 +0530

    learning css,html

---
+++ training/html/APeapcet2025/first.js
@@ -0,0 +1,13 @@
+
+document.getElementById("ResponceSheet").addEventListener("click", function() {
+    window.open("ResponceSheet.html", "_blank", "width=1000,height=700");
+});
+
+document.getElementById("MasterQuestionPaper").addEventListener("click", function() {
+    window.open("MasterQuestionPaper.html", "_blank", "width=1000,height=700");
+});
+
+document.getElementById("Information-button").addEventListener("click", function() {
+    window.open("MasterQuestionPaper.html", "_blank", "width=1000,height=700");
+});
+
