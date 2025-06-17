761ad2a0dd984c27775a9fe3bd6125b74f967253
123Kanishka <kanishka.sriramoju@gmail.com>
2025-06-17 19:42:23 +0530

    updated

---
+++ tasks/Form_Project/index.js
@@ -0,0 +1,5 @@
+var table_vector = document.getElementById("Database");
+var row_vector = document.createElement("tr");
+table_vector.append(row_vector);
+var title = document.getElementById("title");
+title.style.fontSize = '5rem';
--- training/javascript/index.js
+++ training/javascript/index.js
@@ -1,25 +1,92 @@
-var b=10;
-function a(){
-    b+=1;
-    console.log(b);
+// var b=10;
+// function a(){
+//     b+=1;
+//     console.log(b);
+// }
+// a()
+// switch(b){
+//     case 10:
+//         console.log("Hey");
+//         break;
+//     case 11:
+//         console.log("hi")
+//         break;
+//     default:
+//         console.log("bye")
+//         break;
+// }
+// var obj={
+//     "length":25,
+//     "breadth":30,
+//     "name":"Pen",
+//     2:4
+// }
+
+// console.log(obj.hasOwnProperty("length"))
+
+
+// data={
+//     "2051":{
+//         Name:"Kanishka",
+//         age:22,
+//         height:"5'12",
+//         greet:function(){
+//             console.log("Hey there");
+//         }
+
+//     }
+// }
+// console.log(data["2051"]);
+// console.log(JSON.stringify(data["2051"]));
+// console.log(JSON.parse(JSON.stringify(data["2051"])));
+// var myArray=[];
+// for(var i=0;i<10;i++){
+//     myArray.push(i);
+// }
+// console.log(JSON.stringify(myArray));
+// console.log(myArray)
+
+const data=[
+    {
+        Name:"Kanishka",
+        Age:22,
+        ID:1097
+    },
+    {
+        Name:"Uday",
+        Age:21,
+        ID:1197
+    }
+];
+
+function ProfileLookUp(ID_find){
+    for(var i=0;i<data.length;i++){
+        if (data[i].ID==ID_find){
+            return true;
+        }
+    }
+    return false;
 }
-a()
-switch(b){
-    case 10:
-        console.log("Hey");
-        break;
-    case 11:
-        console.log("hi")
-        break;
-    default:
-        console.log("bye")
-        break;
+// console.log(ProfileLookUp(1196));
+
+var n=Math.floor((Math.random()*100));
+function randomRange(min_val, max_val){
+    return Math.floor(Math.random()*(max_val-min_val+1))+min_val
 }
-var obj={
-    "length":25,
-    "breadth":30,
-    "name":"Pen",
-    2:4
+console.log(data[0].Name);
+data[0].Name="Hari";
+console.log(data[0].Name);
+Object.freeze(data[0]);
+data[0].Name="shaik";
+console.log(data[0].Name)
+a=[1,-1,6,7,-2,-5,8]
+console.log(a.filter(num => num>0 && Number.isInteger(num)).map(num => num*num))
+function rest_test(...args){
+    for(var i=0;i<args.length;i++){
+        console.log(args[i])
+    }
 }
-console.log(obj.length);
-console.log(obj[2]);
+rest_test(1,2,"kanishka",2.78,);
+
+//Template Literals
+console.log(`Hi guys ${data[0].Name}"`);
