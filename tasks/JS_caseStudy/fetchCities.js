3bff600da30abe485837dad7970e0790496dff5d
nikasbandaru <prasad.ece3.083@gmail.com>
2025-06-17 19:47:05 +0530

    Js task

---
+++ tasks/JS_caseStudy/fetchCities.js
@@ -0,0 +1,5 @@
+fetch(`https://www.universal-tutorial.com/api/cities/${selectedState}`, {
+  headers: {
+    "Authorization": "Bearer YOUR_ACCESS_TOKEN"
+  }
+})
---
+++ tasks/JS_caseStudy/fetchStates.js
@@ -0,0 +1,5 @@
+fetch('https://www.universal-tutorial.com/api/states/India', {
+  headers: {
+    "Authorization": "Bearer YOUR_ACCESS_TOKEN"
+  }
+})
