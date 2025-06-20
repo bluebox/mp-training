270eebb733761095a6ca62114402e8152897fcda
sahithi <aendapallysahithireddy@gmail.com>
2025-06-20 20:20:56 +0530

    added

--- training/creationJSP/src/main/java/addNumbers.java
+++ training/creationJSP/src/main/java/addNumbers.java
@@ -1,28 +1,29 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-//@WebServlet(urlPatterns="/MyServlet",loadOnStartup = 1)
-@WebServlet("/MyServlet")
-public class MyServlet extends HttpServlet {
+@WebServlet("/addNumbers")
+public class addNumbers extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-//	public void init() {
-//		System.out.println("load on startup is used");
-//	}
-	
-	@Override
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("Form.html");
-        dispatcher.forward(request, response);
+		
+		int a = (Integer)request.getAttribute("a");
+		int b = (Integer)request.getAttribute("b");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print(request.getParameter("number1") + " + ");
+		out.print(request.getParameter("number2") + " = ");
+		out.print(a+b);
 	}
 
 }
--- training/java/servlets/src/main/java/HelloWorldServlet.java
+++ training/java/servlets/src/main/java/HelloWorldServlet.java
@@ -1,5 +1,9 @@
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 //import javax.servlet.annotation.WebServlet;
@@ -13,15 +17,24 @@ public class HelloWorldServlet extends HttpServlet {
 	
     private static final long serialVersionUID = 1L;
 
+    private Connection connection;
+    private static final String url = "jdbc:mysql://localhost:3306/learn";
+    private static final String root = "devuser";
+    private static final String password = "Medplus@123";
+    
     @Override
     public void init() throws ServletException {
         
+    	
     	try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(url,root,password);
 			
 		} 
     	catch (ClassNotFoundException e) {
 			System.out.println("JDBC class not found");
+		} catch (SQLException e) {
+			System.out.println("SQL exception ");
 		}
         System.out.println("Servlet initialized");
     }
@@ -33,6 +46,20 @@ public class HelloWorldServlet extends HttpServlet {
         String place = request.getParameter("place");
         int age = Integer.parseInt(request.getParameter("age"));
         
+        try(PreparedStatement ps = connection.prepareStatement("insert into user(name,email,age,place) values(?,?,?,?);")){
+        	ps.setString(1,username);
+            ps.setString(2,email);
+            ps.setInt(3,age);
+            ps.setString(4,place);
+            int rows = ps.executeUpdate();
+            if(rows > 0) {
+            	System.out.println("row inserted");
+            }
+        }
+        catch(Exception e) {
+//        	System.out.println("error in connection or sql stmt");
+        	e.printStackTrace();
+        }
         response.setContentType("text/html");
         response.getWriter().println("<h3>Received Data:</h3>");
         response.getWriter().println("Name: " + username + "<br>");
@@ -40,4 +67,20 @@ public class HelloWorldServlet extends HttpServlet {
         response.getWriter().println("Age: " + age + "<br>");
         response.getWriter().println("Place: " + place + "<br>");
     }
+	
+	@Override
+	public void destroy() {
+	    try {
+	        if (connection != null && !connection.isClosed()) {
+	            connection.close();
+	            System.out.println("Database connection closed.");
+	        }
+	        com.mysql.cj.jdbc.AbandonedConnectionCleanupThread.checkedShutdown();
+			System.out.println("MySQL Cleanup thread shutdown.");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    super.destroy();
+	}
+
 }
