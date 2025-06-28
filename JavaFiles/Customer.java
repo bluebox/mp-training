38a7706d3b99a4e51f28194de9aa83e5f001d940
Karthik Malasani <karthikmalasani21@email.com>
2025-06-28 11:53:27 +0530

    Java Practise

---
+++ JavaFiles/Customer.java
@@ -0,0 +1,33 @@
+public class Customer {
+
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
+
+    public Customer(String name, double creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+  
+    public Customer() {
+        this("Name", 50000.0, "myeamil@email.com");
+    }
+
+    public Customer(String name, String emailAddress) {
+        this(name, 55000.0, emailAddress);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+}
