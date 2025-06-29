e15fb3d95edfc20fe9e8f585ed6ab37aee391be2
Setty-Karuna <settykaruna798@gmail.com>
2025-06-29 09:49:02 +0530

    Add files via upload

---
+++ CarObj.java
@@ -0,0 +1,14 @@
+
+public class CarObj {
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.setMake("porsche");
+		car.setModel("Carrera");
+		car.setDoors(2);
+		car.setConvertible(true);
+		car.setColor("black");
+		System.out.println("make = " + car.getMake());
+		System.out.println("model = " + car.getModel());
+		System.out.println(car);
+	}
+}
