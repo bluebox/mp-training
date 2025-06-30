71b0e763048838401d86dfb0d71ab1b60473a362
papolukavitha <152165245+papolukavitha@users.noreply.github.com>
2025-06-30 20:38:20 +0530

    Update Wall.java

--- training/java/core/dailytasks/Wall.java
+++ training/java/core/dailytasks/Wall.java
@@ -4,13 +4,13 @@ public class Wall {
     private double width;
     private double height;
 
-    // No-args constructor
+    
     public Wall() {
         this.width = 0;
         this.height = 0;
     }
 
-    // Constructor with parameters
+    
     public Wall(double width, double height) {
         if (width < 0) {
             this.width = 0;
@@ -25,17 +25,17 @@ public class Wall {
         }
     }
 
-    // Method to get width
+  
     public double getWidth() {
         return width;
     }
 
-    // Method to get height
+  
     public double getHeight() {
         return height;
     }
 
-    // Method to set width
+   
     public void setWidth(double width) {
         if (width < 0) {
             this.width = 0;
@@ -44,7 +44,7 @@ public class Wall {
         }
     }
 
-    // Method to set height
+   
     public void setHeight(double height) {
         if (height < 0) {
             this.height = 0;
@@ -53,12 +53,12 @@ public class Wall {
         }
     }
 
-    // Method to get area
+    
     public double getArea() {
         return width * height;
     }
 
-    // Main method for testing and user input
+  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -68,12 +68,12 @@ public class Wall {
         System.out.println("Enter wall height:");
         double userHeight = scanner.nextDouble();
 
-        // Create a Wall object using the parameterized constructor
+        
         Wall wall = new Wall(userWidth, userHeight);
 
         System.out.println("Initial Wall Area: " + wall.getArea());
 
-        // Demonstrate setting new values
+      
         System.out.println("Enter new width:");
         double newWidth = scanner.nextDouble();
         wall.setWidth(newWidth);
