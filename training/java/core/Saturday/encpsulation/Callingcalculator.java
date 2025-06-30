d848b5030fad59815a83c363cff127996a5aee2f
Rohan-bobby <21d41a6956@gmail.com>
2025-06-30 20:46:53 +0530

    Add files via upload

---
+++ training/java/core/Saturday/encpsulation/Callingcalculator.java
@@ -0,0 +1,9 @@
+public class Callingcalculator {
+    public static void main(String[] args) {
+        Simplecalculator sc = new Simplecalculator();
+        sc.setFirstNumber(56);
+        sc.setSecondNumber(23);
+        
+           System.out.println(sc.getAdditionResult());
+    }
+}
