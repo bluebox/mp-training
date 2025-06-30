6ae849eb5461fe1693e583c33d9e80f50f5e545a
papolukavitha <152165245+papolukavitha@users.noreply.github.com>
2025-06-30 23:11:10 +0530

    Update task7.java

--- training/java/core/dailytasks/task7.java
+++ training/java/core/dailytasks/task7.java
@@ -23,14 +23,7 @@ public class task7{
     }
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-       // System.out.println("enter a number to check if it is a odd:");
-        // int testNumber = scanner.nextInt();
-        // if (isOdd(testNumber)){
-        //     System.out.println("the number is odd: ");
-        // }else{
-        //     System.out.println("the number is not odd: ");
-
-        // }
+      
         System.out.println("enter a number  start range must be < 0 : ");
         int startRange = scanner . nextInt();
         System.out.println("enter a number must be end range:  ");
