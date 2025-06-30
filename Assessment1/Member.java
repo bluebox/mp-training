9e857d5300784081fa828773ae5bb37db0b40ded
Pedapudi Lakshmi Pranamya <pedapudipranamya2004@gmail.com>
2025-06-30 16:46:02 +0530

    Add files via upload

---
+++ Assessment1/Member.java
@@ -0,0 +1,25 @@
+class Member extends Person {
+    private String memberId;
+    private MembershipPlan plan;
+
+    public Member(String name, int age, String memberId) {
+        super(name, age);
+        this.memberId = memberId;
+    }
+
+    public void assignPlan(MembershipPlan plan) {
+        this.plan = plan;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("\nMember ID: " + memberId);
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Plan: " + (plan != null ? plan.getDetails() : "Not Assigned"));
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+}
---
+++ Assessment1/MembershipPlan.java
@@ -0,0 +1,19 @@
+class MembershipPlan {
+    private String planName;
+    private int durationMonths;
+    private double fee;
+
+    public MembershipPlan(String planName, int durationMonths, double fee) {
+        this.planName = planName;
+        this.durationMonths = durationMonths;
+        this.fee = fee;
+    }
+
+    public String getDetails() {
+        return planName + " - " + durationMonths + " months - ₹" + fee;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+}
