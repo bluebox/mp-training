1f3228ce1e8e1e12eae8bd17b4ce0a1c34c0614e
papolukavitha <152165245+papolukavitha@users.noreply.github.com>
2025-06-30 20:49:14 +0530

    Update task38.java

--- training/java/core/dailytasks/task38.java
+++ training/java/core/dailytasks/task38.java
@@ -1,10 +1,10 @@
 public static boolean isPerfectNumber(int number) {
-    // Check if the number is less than 1
+    
     if (number < 1) {
         return false;
     }
 
-    // Calculate the sum of proper divisors
+    
     int sumOfDivisors = 0;
     for (int i = 1; i < number; i++) {
         if (number % i == 0) { // Check if 'i' is a divisor of 'number'
@@ -12,6 +12,6 @@ public static boolean isPerfectNumber(int number) {
         }
     }
 
-    // Compare the sum of divisors with the number
+    
     return sumOfDivisors == number;
 }
