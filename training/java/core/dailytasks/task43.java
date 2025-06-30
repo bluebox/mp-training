626869a82e665bab10effe40ba19c7b47eeec29c
papolukavitha <152165245+papolukavitha@users.noreply.github.com>
2025-06-30 23:02:08 +0530

    Update task43.java

--- training/java/core/dailytasks/task43.java
+++ training/java/core/dailytasks/task43.java
@@ -2,18 +2,18 @@ public class LargestPrimeFactor {
 
     public static int getLargestPrime(int number) {
         if (number <= 1) {
-            return -1; // Invalid value for 0, 1, and negative numbers
+            return -1; 
         }
 
         int largestPrime = -1;
 
-        // Divide by 2 until it's no longer divisible
+       
         while (number % 2 == 0) {
             largestPrime = 2;
             number /= 2;
         }
 
-        // Iterate for odd numbers starting from 3
+       
         for (int i = 3; i <= Math.sqrt(number); i += 2) {
             while (number % i == 0) {
                 largestPrime = i;
@@ -21,7 +21,7 @@ public class LargestPrimeFactor {
             }
         }
 
-        // If a prime number greater than 2 remains
+        
         if (number > 2) {
             largestPrime = number;
         }
