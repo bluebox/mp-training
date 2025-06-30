2e491cec1f029d35bc98d4ecfcc963aeab93b3b7
papolukavitha <152165245+papolukavitha@users.noreply.github.com>
2025-06-30 20:39:19 +0530

    Update point.java

--- training/java/core/dailytasks/point.java
+++ training/java/core/dailytasks/point.java
@@ -57,15 +57,15 @@ public class point {
         return Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
     }
 
-    // Main method for testing and user input
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Create a Scanner object [4, 11]
 
-        // Create a Point object using the no-arg constructor
+       
         point p1 = new point();
         System.out.println("Point 1 (default): (" + p1.getX() + ", " + p1.getY() + ")");
 
-        // Get user input for Point 2
+       
         System.out.print("Enter x-coordinate for Point 2: ");
         int x2 = scanner.nextInt();
         System.out.print("Enter y-coordinate for Point 2: ");
@@ -74,17 +74,17 @@ public class point {
 
         System.out.println("Point 2 (user input): (" + p2.getX() + ", " + p2.getY() + ")");
 
-        // Demonstrating methods
+       
         System.out.println("Distance of Point 1 from (0,0): " + p1.distance());
         System.out.println("Distance between Point 1 and Point 2: " + p1.distance(p2));
 
-        // Get user input for a third point to calculate distance
+       
         System.out.print("Enter x-coordinate for a third point: ");
         int x3 = scanner.nextInt();
         System.out.print("Enter y-coordinate for a third point: ");
         int y3 = scanner.nextInt();
         System.out.println("Distance of Point 2 from (" + x3 + ", " + y3 + "): " + p2.distance(x3, y3));
 
-        scanner.close(); // Close the scanner
+        scanner.close(); 
     }
 }
