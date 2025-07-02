e8f0940e6100f7b9f61e3bf91188b2ac31a9d072
saisreeja14 <154822012+saisreeja14@users.noreply.github.com>
2025-07-02 08:14:54 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,18 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car[] cars= {
-				new GasPoweredCar("Tayota",12.5,4),
-				new ElectricCar("Tesla",45.0,75),
-				new HybridCar("Hyundai",25.0,4,50)	
-		};
-		for(Car c:cars)
-		{
-			c.startEngine();
-			c.Drive();
-			c.runEngine();
-		}
+		        Meal meal = new Meal();
+
+		        meal.addBurger("Classic", 150.0, "lettuce", "tomato", "cheese", "bacon");
+		        meal.addBurger("Veggie", 130.0, "lettuce", "tomato", "onion", "avocado");
+
+		        meal.printMeal();
 	}
 
 }
-
