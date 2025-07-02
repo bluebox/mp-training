4b8418f319f51f083c22b03c1b9d7dcd62a9c4f0
Sira Satheesh <satheesh2254@gmail.com>
2025-07-02 18:58:03 +0530

    Case Study : Gym Membership Updated

--- training/java/core/GymMembershipManagementSystem/src/Main.java
+++ training/java/core/GymMembershipManagementSystem/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             System.out.println("2. Assign Membership Plan to Member");
             System.out.println("3. View All Members and Details");
             System.out.println("4. Exit");
+            System.out.println("menu for geting Options");
             System.out.print("Enter your choice: ");
 		
 		try {
@@ -21,30 +22,46 @@ public class Main {
             case 1:
                 System.out.print("Enter Member ID: ");
                 String memberId = sc.nextLine();
+                
+                if(check(memberId)) {
+                	break;
+                }
                 System.out.print("Enter Name: ");
                 String name = sc.nextLine();
-                System.out.print("Enter Age: ");
-                int age = Integer.parseInt(sc.nextLine());
-
-                if (age <= 0) {
-                    System.out.println("Age must be positive.");
-                    break;
+                if(check(name)) {
+                	break;
                 }
-
-                Member newMember = new Member(memberId, name, age);
-                gym.addMember(newMember);
-                break;
+                System.out.print("Enter Age: ");
+                try {
+                	int age = Integer.parseInt(sc.nextLine());
+                
+                	if (age <= 0) {
+                		System.out.println("Age must be positive.");
+                		break;
+                	}
+                	Member newMember = new Member(memberId, name, age);
+                    gym.addMember(newMember);
+                    break;
+                }catch(NumberFormatException e){
+                	break;
+                }
 
             case 2:
                 System.out.print("Enter Member ID to assign plan: ");
                 String idForPlan = sc.nextLine();
+                if(check(idForPlan)) {
+                	break;
+                }
                 System.out.println("Available Plans:");
                 gym.showAvailablePlans();
                 System.out.print("Select plan number: ");
-                int planChoice = Integer.parseInt(sc.nextLine());
-                gym.assignPlanToMember(idForPlan, planChoice);
-                break;
-
+                try {
+	                int planChoice = Integer.parseInt(sc.nextLine());
+	                gym.assignPlanToMember(idForPlan, planChoice);
+	                break;
+                }catch(NumberFormatException e){
+                	break;
+                }
             case 3:
                 gym.viewAllMembers();
                 break;
@@ -58,11 +75,19 @@ public class Main {
                 System.out.println("Invalid choice. Try again.");
         }
 		}catch (NumberFormatException e) {
-			System.out.println("Invalid input. Please enter numbers only.");
+			System.out.println("Please enter numbers Choice.");
         }catch (Exception e) {
             System.out.println("An unexpected error occurred: " + e.getMessage());
         }
 		}
 		sc.close();
+	}
+	
+	public static boolean check(String option) {
+		if(option.toLowerCase().equals("menu")) {
+			return true;
+		}
+		return false;
+		
 	}
 }
