d9029af5825e4e9a8935e8025dbc551c96e4899d
Vardhan Rangineni <ranginenivardhan@gmail.com>
2025-07-04 13:42:49 +0530

    Day 6 Challenges

---
+++ training/java/core/Day6_/MiniChallange/MiniChallenge1.java
@@ -0,0 +1,12 @@
+package MiniChallange;
+
+import java.util.function.Consumer;
+
+public class MiniChallenge1 {
+	static Consumer<String> printTheParts = sentence -> {
+		String parts[] = sentence.split(" ");
+		for (String part : parts) {
+			System.out.println(part);
+		}
+	};
+}
---
+++ training/java/core/Day6_/MiniChallange/MiniChallenge7.java
@@ -0,0 +1,7 @@
+package MiniChallange;
+
+import java.util.function.Supplier;
+
+public class MiniChallenge7 {
+	public static Supplier<String> iLoveJava = () -> "iLoveJava";
+}
