5ecd8fcb480725a396918ce43e3887dc91c289ec
Akash Madhunala <166631596+Akashmadhunala@users.noreply.github.com>
2025-07-07 09:23:41 +0530

    Updated

--- JavaPractise/SetOperationsChallenge/src/Main.java
+++ JavaPractise/SetOperationsChallenge/src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("All Tasks:");
         TaskData.getTasks("all").forEach(System.out::println);
-
         System.out.println("Akash's Tasks:");
         TaskData.getTasks("Akash").forEach(System.out::println);
-
         System.out.println("B's Tasks:");
         TaskData.getTasks("B").forEach(System.out::println);
     }
