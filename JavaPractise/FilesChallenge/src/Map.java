67660f32b66ccd20ba64f21be5e893864afd59eb
Akash Madhunala <166631596+Akashmadhunala@users.noreply.github.com>
2025-07-10 13:01:17 +0530

    Updated

--- JavaPractise/FilesChallenge/src/Map.java
+++ JavaPractise/FilesChallenge/src/Map.java
@@ -1,27 +1,23 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 public class Map {
-
 	public static void main(String[]Lists) {
-		HashMap<String,List<Integer>> map =new HashMap<>();
-		
-//		map.put("Odd",new ArrayList<>());
-//		map.put("Even",new ArrayList<>());
+		HashMap<String,List<Integer>> map =new HashMap<>();	
+		map.put("Odd",new ArrayList<>());
+		map.put("Even",new ArrayList<>());
 		for(int i=0;i<=100;i++) {
 			if(i%2==0) {
-				List<Integer> even = map.getOrDefault("even", new ArrayList<>());
-				even.add(i);
-				map.putIfAbsent("even", even);
+				 map.get("even").add(i);
 			}
 			else {
-				List<Integer> odd = map.getOrDefault("odd", new ArrayList<>());
-				odd.add(i);
-				map.putIfAbsent("odd", odd);
+			        map.get("odd").add(i);
 			}
 		}
-
+		ArrayList<Integer> odd = map.get("odd");
+		ArrayList<Integer> even=map.get("even");
+		System.out.println("The Odd numbers are :"+odd);
+		System.out.println("The even numbers are :"+even);
 	}
 
 }
