21f2d0257f24a2a5d78fbc33ac6b02fc6db9b9e4
Dasari Devi Sri Vara Prasad <dasaridevisrivaraprasad@gmail.com>
2025-07-11 20:07:39 +0530

    added

--- training/python/PythonProject2/myproject/myproject/settings.py
+++ training/python/PythonProject2/myproject/myproject/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+  'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'college',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'Medplus@123',
     }
+
 }
 
 
--- training/python/PythonProject2/myproject/sample/models.py
+++ training/python/PythonProject2/myproject/sample/models.py
@@ -1,3 +1,75 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=255)
+    dept_id = models.IntegerField()
+    credits = models.DecimalField(max_digits=3, decimal_places=1)
+
+    class Meta:
+        managed = False
+        db_table = 'course'
+
+
+class CourseAssignment(models.Model):
+    course_id = models.IntegerField(primary_key=True)  # The composite primary key (course_id, teacher_id) found, that is not supported. The first column is selected.
+    teacher_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'course_assignment'
+        unique_together = (('course_id', 'teacher_id'),)
+
+
+class CourseEnrollment(models.Model):
+    student_id = models.IntegerField()
+    course_id = models.IntegerField()
+    teacher_id = models.IntegerField()
+    internal_marks = models.DecimalField(max_digits=3, decimal_places=1)
+    external_marks = models.DecimalField(max_digits=3, decimal_places=1)
+
+    class Meta:
+        managed = False
+        db_table = 'course_enrollment'
+
+
+class Department(models.Model):
+    dept_id = models.AutoField(primary_key=True)
+    dept_name = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'department'
+
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    enrollment_year = models.TextField()  # This field type is a guess.
+    dept_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'student'
+
+
+class Teacher(models.Model):
+    teacher_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    dept_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'teacher'
