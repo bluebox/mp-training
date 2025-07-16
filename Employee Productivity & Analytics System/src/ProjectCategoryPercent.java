6278a5efc40b042748ab520930d56bec897137ed
saisreeja14 <154822012+saisreeja14@users.noreply.github.com>
2025-07-16 14:30:45 +0530

    Delete Employee Productivity & Analytics System directory

--- Employee Productivity & Analytics System/src/ProjectCategoryPercent.java
+++
@@ -1,8 +0,0 @@
- class ProjectCategoryPercent {
-    String projectId;
-    String category;
-    double percent;
-    ProjectCategoryPercent(String p, String c, double pct) {
-        projectId = p; category = c; percent = pct;
-    }
-}
