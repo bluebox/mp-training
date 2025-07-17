fce3bde654f5e7f24edcfca7e72efb93d715c365
Akashmadhunala <akashmadhunala.com>
2025-07-17 20:07:29 +0530

    17-07-2025

---
+++ JavaPractise/UnitTesting/src/Main.java
@@ -0,0 +1,11 @@
+class Calci{
+	public  int add(int a ,int b) {
+		return a+b;
+	}
+	public  int sub(int a ,int b) {
+		return a-b;
+	}
+	public  int multiplication(int a ,int b) {
+		return a*b;
+	}
+}
