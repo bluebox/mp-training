386447136410f356b8862f2ec3c95e3e2ab7842a
Santhoshkumar135 <139996385+Santhoshkumar135@users.noreply.github.com>
2025-07-23 20:52:47 +0530

    Update ServiceHandler.java

--- Case_Study/Gym_membership/src/Services/ServiceHandler.java
+++ Case_Study/Gym_membership/src/Services/ServiceHandler.java
@@ -1,12 +1,19 @@
 
 package Services;
 
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
 import DAO.Databasemanager;
 
 public class ServiceHandler {
 	Databasemanager dbmanager=new Databasemanager();
+	public List<MembershipPlan> membershipplans=new ArrayList<>();
+	
 	Gym gym=new Gym();
-	public void addmember(String name,int age,int memberid) {
+	public void addmember(String name,int age,int memberid) throws SQLIntegrityConstraintViolationException {
 		Member member=new Member(name, age, memberid);
 		boolean memberexist=dbmanager.addmemberstodatabase(member);
 		if(!memberexist)
@@ -14,24 +21,35 @@ public class ServiceHandler {
         //member.show_details();
 		
 	}
-	public void addmembership(int memberid,String planname,int planduration,int fee) {
-		 MembershipPlan membershipplan=new MembershipPlan(planname, planduration, fee);
-         dbmanager.addmembership(memberid, membershipplan);
-		 gym.assignPlan(memberid, membershipplan);
+	public boolean addmembership(int memberid,int membershipplanid) {
+		
+         return dbmanager.addmembership(memberid, membershipplanid);
+         
+        
+		 //gym.assignPlan(memberid, membershipplanid);
 	}
-	public void showallmembers() {
-		dbmanager.showmembersdb();
+	public List<Member> showallmembers() throws SQLIntegrityConstraintViolationException {
+		return dbmanager.showmembersdb();
 		//gym.show_All_Members();
 	}
 	public void updatemembership(int memberid,String planname,int duration,int fee) {
 		dbmanager.updatemembershipplan(memberid,planname,duration,fee);
 	}
-	public void addplans(String planname,int duration,int fee) {
-		dbmanager.addmemberplans(new MembershipPlan(planname,duration,fee));
+	public void addplans(String planname,int duration,int fee) throws SQLIntegrityConstraintViolationException {
+		dbmanager.addmemberplans(new MembershipPlan(planname,duration,fee,0));
 		
 	}
-	public void deletemembership(int memberid) {
-		dbmanager.deletemembershipidfromdatabase(memberid);
+	public boolean  deletemembership(int memberid) {
+		 return dbmanager.deletemembershipidfromdatabase(memberid);
+	}
+	public void showplans() {
+		 
+		 
+		membershipplans=dbmanager.showplans();
+		 System.out.println(membershipplans);
+		 for(MembershipPlan m:membershipplans) {
+			 System.out.println("name"+m.getPlanName());
+		 }
 	}
 	
 	
