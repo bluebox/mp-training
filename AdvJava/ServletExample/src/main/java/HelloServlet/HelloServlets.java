64a9355ef4985fe148221657f8c03828315df8b4
MALASANI KARTHIK <karthikmalasani21@gmail.com>
2025-07-30 14:41:31 +0530

    Task

---
+++ AdvJava/ServletExample/src/main/java/HelloServlet/HelloServlets.java
@@ -0,0 +1,21 @@
+package HelloServlet;
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlets extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Servlet!</h1>");
+    }
+}
