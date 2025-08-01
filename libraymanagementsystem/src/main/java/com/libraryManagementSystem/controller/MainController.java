f907e68bdb1247175b684b25511ba11bbeb51251
charan11368 <charancharan11368@gmail.com>
2025-08-01 12:16:16 +0530

    added library management system

---
+++ libraymanagementsystem/src/main/java/com/libraryManagementSystem/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.libraryManagementSystem.controller;
+
+import java.io.IOException;
+
+import com.libraryManagementSystem.App;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+	@FXML
+	public void switchToBook() throws IOException {
+
+		App.setRoot("BookOptions");
+	}
+
+	@FXML
+	public void switchToMemberOptions() throws IOException {
+		App.setRoot("Members");
+	}
+
+	@FXML
+	public void switchTOIssueAndReturn() throws Exception {
+		App.setRoot("Issue&Return");
+	}
+
+	@FXML
+	public void switchToReports() throws Exception {
+		App.setRoot("Reports");
+	}
+}
---
+++ libraymanagementsystem/src/main/java/com/libraryManagementSystem/dao/BookDao.java
@@ -0,0 +1,36 @@
+package com.libraryManagementSystem.dao;
+
+import java.util.List;
+
+import com.libraryManagementSystem.domain.Book;
+import com.libraryManagementSystem.domain.CustomActiveIssuedBooks;
+import com.libraryManagementSystem.domain.CustomCategoryCount;
+import com.libraryManagementSystem.domain.CustomOverDueBooks;
+import com.libraryManagementSystem.exceptions.InvalidException;
+import com.libraryManagementSystem.utilities.BookAvailability;
+
+public interface BookDao {
+
+	void addBook(Book book) throws InvalidException;
+
+	void updateBook(Book book, Book oldBook) throws InvalidException;
+
+	void updateBookAvalability(Book book, BookAvailability avalability) throws InvalidException;
+
+	boolean getBookByTitleAndAuthor(String title, String author) throws InvalidException;
+
+	List<Book> getAllBooks() throws InvalidException;
+
+	void deleteBook(Book book) throws InvalidException;
+
+	void bookLog(Book book) throws InvalidException;
+
+	Book getBookById(int id) throws InvalidException;
+
+	List<CustomCategoryCount> getBookCountByCategory() throws InvalidException;
+
+	List<CustomActiveIssuedBooks> getActiveIssuedBooks() throws InvalidException;
+
+	List<CustomOverDueBooks> getOverDueBooks() throws InvalidException;
+
+}
