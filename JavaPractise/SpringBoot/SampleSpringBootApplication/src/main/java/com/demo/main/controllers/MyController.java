8958fde52f328bad6ed46d48a53be32f268255d5
Akashmadhunala <akashmadhunala.com>
2025-08-08 14:24:36 +0530

    Basic Spring Boot Application

---
+++ JavaPractise/SpringBoot/SampleSpringBootApplication/src/main/java/com/demo/main/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.demo.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/home")
+	public String getMessage() {
+		return "home.html";
+	}
+}
