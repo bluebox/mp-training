2973a613748e2cbfce399d1d95a6c78e950ffa04
Pedapudi Lakshmi Pranamya <pedapudipranamya2004@gmail.com>
2025-08-12 18:53:01 +0530

    Add files via upload

---
+++ 12-08-2025/WebConfig.java
@@ -0,0 +1,16 @@
+package com.mycompany.Company.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/about").setViewName("about");
+        registry.addViewController("/services").setViewName("services");
+    }
+
+}
