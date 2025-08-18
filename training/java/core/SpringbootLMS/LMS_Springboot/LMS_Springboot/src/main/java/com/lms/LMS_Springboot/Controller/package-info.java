9b526eac3298c0eabdeb52500e03ef7bc2f7db07
Rohan-bobby <21d41a6956@gmail.com>
2025-08-18 13:04:39 +0530

    Delete training/java/core/SpringbootLMS/LMS_Springboot directory

--- training/java/core/SpringbootLMS/LMS_Springboot/LMS_Springboot/src/main/java/com/lms/LMS_Springboot/Controller/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.lms.LMS_Springboot.Controller;
--- training/java/core/SpringbootLMS/LMS_Springboot/LMS_Springboot/src/main/java/com/lms/LMS_Springboot/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lms.LMS_Springboot.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
